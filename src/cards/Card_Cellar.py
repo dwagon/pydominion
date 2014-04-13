d925f5899f516321bc6bcfd83e539cc5e3357507
Dougal Scott <Dougal.Scott@medibankhealth.com.au>
2014-04-14 08:07:27 +1000

    Developed Cellar

--- src/cards/Card_Cellar.py
+++ src/cards/Card_Cellar.py
@@ -1,5 +1,6 @@
 from Card import Card
 
+
 class Card_Cellar(Card):
     def __init__(self):
         Card.__init__(self)
@@ -10,6 +11,38 @@ class Card_Cellar(Card):
         self.cost = 2
 
     def special(self, game, player):
-        print "Not Implemented"
+        """ Discard any number of cards, +1 card per card discarded """
+        options = [{'selector': '0', 'print': 'Discard no more', 'card': None}]
+        todiscard = []
+        index = 1
+        for c in player.hand:
+            s = "%s" % index
+            options.append({'selector': s, 'print': 'Discard %s' % c.name, 'card': c})
+            index += 1
+
+        for o in options:
+            print "%s\t%s" % (o['selector'], o['print'])
+        while(1):
+            print "Select which card(s) to discard?",
+            input = raw_input()
+            good = False
+            if input == '0':
+                break
+            for o in options:
+                if o['selector'] == input:
+                    good = True
+                    if o['card'] in todiscard:
+                        todiscard.remove(o['card'])
+                    else:
+                        todiscard.append(o['card'])
+                    break
+            if not good:
+                print "Invalid Option (%s) - '0' to stop discarding" % input
+
+        for c in todiscard:
+            print "Discarding %s" % c.name
+            player.addCard(c, 'discard')
+            n = player.pickupCard()
+            print "Picked up %s" % n.name
 
 #EOF
