5358d0a6abce8cf08497be7c9e822558bbb8b66c
Dougal Scott <Dougal.Scott@medibankhealth.com.au>
2014-04-13 21:33:38 +1000

    Added adventurer code

--- src/cards/Card_Adventurer.py
+++ src/cards/Card_Adventurer.py
@@ -9,6 +9,15 @@ class Card_Adventurer(Card):
         self.cost = 6
 
     def special(self, game, player):
-        print "Not Implemented"
+        """ Reveal cards from your deck until you reveal two treasure cards
+        Add those to your hand and discard the other revealed cards """
+        treasures = []
+        while len(treasures)<2:
+            c = player.pickupCard()
+            if c.isTreasure():
+                treasures.append(c)
+                print "Adding %s" % c.name
+            else:
+                player.discardCard(c)
 
 #EOF
