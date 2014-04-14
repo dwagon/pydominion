f6b7923dc8122bd79f3b7c65bc771060fd749f62
Dougal Scott <Dougal.Scott@medibankhealth.com.au>
2014-04-14 18:10:44 +1000

    Implemented Chancellor

--- src/cards/Card_Gardens.py
+++ src/cards/Card_Gardens.py
@@ -1,14 +1,19 @@
 from Card import Card
 
+
 class Card_Gardens(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'victory'
         self.name = 'gardens'
+        self.playable = False
         self.image = 'images/gardens.jpg'
         self.cost = 4
 
     def special(self, game, player):
-        print "Not Implemented"
+        pass
+
+    def special_score(self):
+        print "Not implemented"
 
 #EOF
