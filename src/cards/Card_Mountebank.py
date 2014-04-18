700f2715d4171d0e94145f214837cf3731bfc9cd
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-18 13:19:41 +1000

    Implemented mountebank

--- src/cards/Card_Mountebank.py
+++ src/cards/Card_Mountebank.py
@@ -13,6 +13,20 @@ class Card_Mountebank(Card):
 
     def special(self, game, player):
         """ Each other player may discard a Curse. If he doesnt, he gains a Curse and a Copper """
-        print "Not implemented yet"
+        for plr in game.players:
+            if plr == player:
+                continue
+            if plr.hasDefense():
+                print "Player %s is defended" % plr.name
+                continue
+            for c in plr.hand:
+                if c.cardname == 'curse':
+                    print "Player %s trashed a curse" % plr.name
+                    plr.trashCard(c)
+                    break
+            else:
+                print "Player %s gained a curse and a copper" % plr.name
+                plr.addCard(game['Curse'].remove())
+                plr.addCard(game['Copper'].remove())
 
 #EOF
