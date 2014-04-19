f9f97d15d5cef432f07145e9d7a7fd88766e7b9c
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-20 07:58:19 +1000

    Added throne room card

---
+++ src/cards/Card_Throneroom.py
@@ -0,0 +1,31 @@
+from Card import Card
+
+
+class Card_Throneroom(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "Play action 2 times"
+        self.name = "Throne Room"
+        self.cost = 4
+
+    def special(self, game, player):
+        """ You may chose an Action card in your hand. Play it twice """
+        options = [{'selector': '0', 'print': "Don't play a card", 'card': None}]
+        index = 1
+        for c in player.hand:
+            if not c.isAction():
+                continue
+            sel = "%d" % index
+            pr = "Play %s twice" % c.name
+            options.append({'selector': sel, 'print': pr, 'card': c})
+            index += 1
+        o = player.userInput(options, "Play which action card twice?")
+        if not o['card']:
+            return
+        for i in range(1, 3):
+            print "Number %d play of %s" % (i, o['card'].name)
+            player.playCard(o['card'], discard=False, costAction=False)
+        player.discardCard(o['card'])
+
+#EOF
