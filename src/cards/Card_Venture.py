a448155c7169564da83f8266e9556628d5bc2cb5
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-19 13:21:00 +1000

    Implemented Venture

--- src/cards/Card_Venture.py
+++ src/cards/Card_Venture.py
@@ -13,6 +13,14 @@ class Card_Venture(Card):
         """ When you play this, reveal cards from your deck until
             you reveal a Treasure. Discard the other cards. Play that
             Treasure """
-        print "Not implemented yet"
+        while(1):
+            c = player.pickupCard()
+            if c.isTreasure():
+                print "Picked up %s" % c.name
+                break
+            else:
+                print "Picked up and discarded %s" % c.name
+                self.t['gold'] -= c.gold    # Compensate for not keeping card
+                player.discardCard(c)
 
 #EOF
