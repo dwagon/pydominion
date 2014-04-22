13260f9a6c36759908d8fb6ddb265f18d8a0f3c6
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-22 21:17:42 +1000

    Added and implemented great hall

---
+++ src/cards/Card_Greathall.py
@@ -0,0 +1,17 @@
+from Card import Card
+
+
+class Card_Greathall(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "+1 card, +1 action, 1VP"
+        self.name = 'Great Hall'
+        self.cost = 5
+        self.cards = 1
+        self.actions = 1
+
+    def special_score(self, game, player):
+        return 1
+
+#EOF
