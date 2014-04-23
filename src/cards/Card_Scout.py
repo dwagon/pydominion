e0ec59298ab592bacc43616dcd926cb4deb661d6
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-23 18:07:42 +1000

    Added and implemented scout

---
+++ src/cards/Card_Scout.py
@@ -0,0 +1,29 @@
+from Card import Card
+
+
+class Card_Scout(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "+1 action, Adjust top 4 cards of deck"
+        self.name = 'Scout'
+        self.actions = 1
+        self.cost = 4
+
+    def special(self, game, player):
+        """ Reveal the top 4 cards of your deck. Put the revealed
+            victory cards into your hand. Put the other cards on top
+            of your deck in any order """
+        #TODO: Currently you can't order the cards you return
+        cards = []
+        for i in range(4):
+            c = player.nextCard()
+            if c.isVictory():
+                player.addCard(c, 'hand')
+                print "Adding %s to hand" % c.name
+            else:
+                cards.append(c)
+        for c in cards:
+            print "Putting %s back on deck" % c.name
+            player.addCard(c, 'deck')
+#EOF
