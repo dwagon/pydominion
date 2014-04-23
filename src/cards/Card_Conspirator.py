6d47453edb6d0630b53d2b5dccc6128ddc4a3ae4
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-23 18:19:39 +1000

    Added and implemented conspirator

---
+++ src/cards/Card_Conspirator.py
@@ -0,0 +1,21 @@
+from Card import Card
+
+
+class Card_Conspirator(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "+2 gold. If played more than 3 actions +1 card, +1 action"
+        self.name = 'Conspirator'
+        self.gold = 2
+        self.cost = 4
+
+    def special(self, player, game):
+        """ If you've player 3 or more actions this turn (counting
+            this); +1 card, +1 action """
+        if player.turnstats['actions'] >= 3:
+            c = player.pickupCard()
+            print "Picked up %s" % c.name
+            player.t['actions'] += 1
+
+#EOF
