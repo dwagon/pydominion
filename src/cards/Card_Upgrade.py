45a1ee2b8bc66772fbf0ba6450c91b73d3a2a1e8
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-23 22:15:51 +1000

    Added Upgrade

---
+++ src/cards/Card_Upgrade.py
@@ -0,0 +1,22 @@
+from Card import Card
+
+
+class Card_Upgrade(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "+1 card, +1 action. Trash a card and gain one costing 1 more"
+        self.name = 'Upgrade'
+        self.cards = 1
+        self.actions = 1
+        self.cost = 5
+
+    def special(self, game, player):
+        """ Trash a card from your hand. Gain a card costing up to 1 more than it """
+        print "Trash a card from your hand. Gain a card costing exactly 1 more than it"
+        tc = player.plrTrashCard(printcost=True)
+        if tc:
+            cost = tc.cost
+            player.plrGainCard(cost + 1, 'equal')
+
+#EOF
