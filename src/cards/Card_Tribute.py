61e9bd8ba17599d0f6e4e134ff1c29e39f8f39d1
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-25 09:55:33 +1000

    Added tribute

---
+++ src/cards/Card_Tribute.py
@@ -0,0 +1,37 @@
+from Card import Card
+
+
+class Card_Tribute(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "Player to left discards 2 cards; you get goodies"
+        self.name = 'Tribute'
+        self.cost = 5
+
+    def special(self, game, player):
+        """ The player to your left reveals then discards the top
+            2 cards of his deck. For each differently named card revealed,
+            if is an Action card, +2 actions; treasure card, +2 gold;
+            victory card, +2 cards """
+        victim = game.playerToLeft(player)
+        cards = [victim.nextCard(), victim.nextCard()]
+        cardname = None
+        for c in cards:
+            print "Looking at %s from %s" % (c.name, victim.name)
+            victim.addCard(c, 'discard')
+            if c.name == cardname:
+                continue
+            cardname = c.name
+            if c.isAction():
+                print "Gained two actions"
+                player.t['actions'] += 2
+            elif c.isTreasure():
+                print "Gained two gold"
+                player.t['gold'] += 2
+            elif c.isVictory():
+                for i in range(2):
+                    n = player.pickupCard()
+                    print "Picked up %s" % n.name
+
+#EOF
