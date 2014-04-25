69c8bd7f9e3b9d3e4aa07788bda8bbe4b92e8742
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-26 09:22:57 +1000

    Implemented coppersmith

---
+++ src/cards/Card_Coppersmith.py
@@ -0,0 +1,18 @@
+from Card import Card
+
+class Card_Coppersmith(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "Copper produces and extra +1 this turn"
+        self.name = 'Coppersmith'
+        self.cost = 4
+
+    def hook_spendvalue(self, game, player, card):
+        """ Copper produces an extra 1 this turn """
+        if card.name == 'Copper':
+            print "Copper worth 1 more"
+            return 1
+        return 0
+
+#EOF
