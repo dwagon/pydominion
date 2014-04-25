db999d5c800db00cf4a87d8ba788d9d7bf92ab5c
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-25 10:53:26 +1000

    Fix description

--- src/cards/Card_Shantytown.py
+++ src/cards/Card_Shantytown.py
@@ -5,7 +5,7 @@ class Card_Shantytown(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'action'
-        self.desc = "If no action in hand, +2 cards"
+        self.desc = "+2 actions. If no action in hand, +2 cards"
         self.name = 'Shanty Town'
         self.actions = 2
         self.cost = 3
