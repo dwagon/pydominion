a010167a03d71ed69a1eb3a217aa4bd173fcaa01
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-26 17:58:10 +1000

    Implemented University

---
+++ src/cards/Card_University.py
@@ -0,0 +1,17 @@
+from Card import Card
+
+
+class Card_University(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "Gain an action card costing up to 5"
+        self.name = 'University'
+        self.cost = 2
+        self.potcost = 1
+
+    def special(self, game, player):
+        """ Gain an action card costing up to 5"""
+        player.plrGainCard(5, actiononly=True)
+
+#EOF
