14cfa86605d8fc8b632b6cbc2f072517af056884
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-26 20:44:44 +1000

    Added Apothecary

---
+++ src/cards/Card_Apothecary.py
@@ -0,0 +1,32 @@
+from Card import Card
+
+
+class Card_Apothecary(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "+1 card, +1 action, Take coppers and potions out of top 4 of deck"
+        self.name = 'Apothecary'
+        self.cards = 1
+        self.actions = 1
+        self.cost = 2
+        self.potcost = 1
+
+    def special(self, player, game):
+        """ Reveal the top 4 cards of your deck. Put the revealed
+            Coppers and Potions into your gand. Put the other cards
+            back on top of your deck in any order """
+        unput = []
+        for i in range(4):
+            c = player.nextCard()
+            if c.name in ('Copper', 'Potion'):
+                print "Putting %s in hand " % c.name
+                player.addCard(c, 'hand')
+            else:
+                unput.append(c)
+        for c in unput:
+            print "Putting %s back in deck" % c.name
+            player.addCard(c, 'deck')
+
+
+#EOF
