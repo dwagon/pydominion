39e547fcca5a918fd43d2238d8a47156b8d371db
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-27 10:23:14 +1000

    Added familiar

---
+++ src/cards/Card_Familiar.py
@@ -0,0 +1,24 @@
+from Card import Card
+
+
+class Card_Familiar(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "+1 card, +1 action, curse everyone else"
+        self.needcurse = True
+        self.name = 'Familiar'
+        self.cards = 1
+        self.actions = 1
+        self.cost = 3
+        self.potcost = 1
+
+    def special(self, game, player):
+        """ All other players gain a curse """
+        for pl in game.players:
+            if pl != player:
+                if not pl.hasDefense():
+                    player.output("%s got cursed" % pl.name)
+                    pl.gainCard('curse')
+
+#EOF
