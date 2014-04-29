212fde197b5a0825facda20f73fcacaca194f2d9
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-29 21:35:22 +1000

    Added embassy

---
+++ src/cards/Card_Embassy.py
@@ -0,0 +1,23 @@
+from Card import Card
+
+
+class Card_Embassy(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "+5 Cards, Discard 3. Everyone gets a silver on purchase"
+        self.name = 'Embassy'
+        self.cost = 5
+        self.cards = 5
+
+    def special(self, game, player):
+        player.plrDiscardCards(3)
+
+    def hook_purchasedCard(self, game, player):
+        """ When you gain this, each other player gains a Silver """
+        for plr in game.players:
+            if plr != player:
+                plr.output("Gained a silver from %s's purchase of Embassy" % player.name)
+                plr.gainCard('Silver')
+
+#EOF
