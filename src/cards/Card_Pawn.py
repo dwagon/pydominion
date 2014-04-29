5d899509fed591baace0322fc07fc3441deb3ca8
Dougal Scott <Dougal.Scott@gmail.com>
2014-04-30 07:56:25 +1000

    Added Pawn

---
+++ src/cards/Card_Pawn.py
@@ -0,0 +1,32 @@
+from Card import Card
+
+
+class Card_Pawn(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.desc = "Choose two: +1 card, +1 action, +1 buy, +1 gold"
+        self.name = 'Pawn'
+        self.cost = 2
+
+    def special(self, game, player):
+        """ Choose two: +1 card; +1 action +1 buy; +1 gold. (The choices myst be different)"""
+        selectable = {'card': '+1 card', 'action': '+1 action', 'buy': '+1 buy', 'gold': '+1 gold'}
+        for i in range(2):
+            options = []
+            index = 0
+            for k, v in selectable.items():
+                options.append({'selector': '%d' % index, 'print': v, 'opt': k})
+                index += 1
+            o = player.userInput(options, "What do you want to do?")
+            if o['opt'] == 'card':
+                player.pickupCard()
+            elif o['opt'] == 'action':
+                player.t['action'] += 1
+            elif o['opt'] == 'buy':
+                player.t['buys'] += 1
+            elif o['opt'] == 'gold':
+                player.t['gold'] += 1
+            del selectable[o['opt']]
+
+#EOF
