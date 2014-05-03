2316f50214de07c38215e7a06c5e8b4018793a09
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-03 22:24:59 +1000

    Added Adventurer tests

--- src/cards/Card_Adventurer.py
+++ src/cards/Card_Adventurer.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Adventurer(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'action'
         self.base = 'dominion'
-        self.desc = "Discard until two treasures"
+        self.desc = "Dig through deck for two treasures"
         self.name = 'Adventurer'
         self.cost = 6
 
@@ -21,5 +25,27 @@ class Card_Adventurer(Card):
                 player.output("Adding %s" % c.name)
             else:
                 player.discardCard(c)
+
+
+###############################################################################
+class Test_Adventurer(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=2, initcards=['adventurer'])
+        self.plr = self.g.players[0]
+
+    def test_treasures(self):
+        self.plr.setDeck('copper', 'silver', 'gold', 'estate')
+        self.plr.setHand('adventurer')
+        self.plr.playCard(self.plr.hand[0])
+        self.assertEquals(sorted(['Silver', 'Gold']), sorted([c.name for c in self.plr.hand]))
+        self.assertEquals(self.plr.discardpile[0].name, 'Estate')
+        self.assertEquals(self.plr.deck[0].name, 'Copper')
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
