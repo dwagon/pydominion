546e5e70d69d84e4597a68723b35fe58de2b2b57
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-04 08:45:49 +1000

    Testing councilroom

--- src/cards/Card_Councilroom.py
+++ src/cards/Card_Councilroom.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -16,6 +19,29 @@ class Card_Councilroom(Card):
         """ Each other player draws a card """
         for pl in game.players:
             if pl != player:
+                pl.output("Picking up card due to %s playing a councilroom" % player.name)
                 pl.pickupCard()
+
+
+###############################################################################
+class Test_Councilroom(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=2, initcards=['councilroom'])
+        self.plr = self.g.players[0]
+        self.ccard = self.g['councilroom'].remove()
+        self.plr.addCard(self.ccard, 'hand')
+
+    def test_play(self):
+        self.assertEquals(len(self.g.players[1].hand), 5)
+        self.plr.playCard(self.ccard)
+        self.assertEquals(len(self.g.players[1].hand), 6)
+        self.assertEquals(len(self.g.players[0].hand), 9)
+        self.assertEquals(self.plr.t['buys'], 2)
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
