82aa69a7f092f6efde46624a7a755bf4b209c2d7
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-04 08:36:22 +1000

    Testing chapel

--- src/cards/Card_Chapel.py
+++ src/cards/Card_Chapel.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -37,5 +40,34 @@ class Card_Chapel(Card):
         for t in trash:
             player.output("Trashing %s" % t.name)
             player.trashCard(t)
+
+
+###############################################################################
+class Test_Chapel(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['chapel'])
+        self.plr = self.g.players[0]
+        self.ccard = self.g['chapel'].remove()
+        self.plr.setHand('estate', 'estate', 'estate')
+        self.plr.addCard(self.ccard, 'hand')
+
+    def test_trashnone(self):
+        self.plr.test_input = ['0']
+        self.plr.playCard(self.ccard)
+        self.assertEquals(len(self.plr.hand), 3)
+        self.assertEquals(self.g.trashpile, [])
+
+    def test_trashtwo(self):
+        self.plr.test_input = ['1', '2', '0']
+        self.plr.playCard(self.ccard)
+        self.assertEquals(len(self.plr.hand), 1)
+        self.assertEquals(len(self.g.trashpile), 2)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
