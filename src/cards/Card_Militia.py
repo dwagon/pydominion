c683c11e0cb649d30a6d816b74dbdb3a3041e949
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-04 18:25:17 +1000

    Testing Militia

--- src/cards/Card_Militia.py
+++ src/cards/Card_Militia.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -20,5 +23,36 @@ class Card_Militia(Card):
                 continue
             plr.output("Discard down to 3 cards")
             plr.plrDiscardDownTo(3)
+
+
+###############################################################################
+class Test_Militia(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=2, initcards=['militia', 'moat'])
+        self.attacker = self.g.players[0]
+        self.defender = self.g.players[1]
+        self.mcard = self.g['militia'].remove()
+
+    def test_defense(self):
+        self.attacker.addCard(self.mcard, 'hand')
+        self.defender.addCard(self.g['moat'].remove(), 'hand')
+        #self.defender.test_input = ['1', '1', '0']
+        self.attacker.playCard(self.mcard)
+        self.assertEquals(len(self.defender.hand), 6)   # Normal + moat
+        self.assertEquals(self.attacker.t['gold'], 2)
+
+    def test_attack(self):
+        self.attacker.addCard(self.mcard, 'hand')
+        self.defender.test_input = ['1', '2', '0']
+        self.attacker.playCard(self.mcard)
+        self.assertEquals(len(self.defender.hand), 3)   # Normal  - 2
+        self.assertEquals(len(self.defender.discardpile), 2)
+        self.assertEquals(self.attacker.t['gold'], 2)
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
