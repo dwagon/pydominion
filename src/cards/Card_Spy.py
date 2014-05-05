174564d0bac3a77afc3fe92dd3c3269d915aa3c7
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-06 07:59:45 +1000

    Testing spy

--- src/cards/Card_Spy.py
+++ src/cards/Card_Spy.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Spy(Card):
     def __init__(self):
         Card.__init__(self)
@@ -15,18 +19,59 @@ class Card_Spy(Card):
     def special(self, game, player):
         """ Each player (including you) reveals the top of his deck and either discards it or puts it back, your choice"""
         for pl in game.players:
-            if not pl.hasDefense(player):
-                self.spyOn(pl)
+            if not pl.hasDefense(player) or player == pl:
+                self.spyOn(player, pl)
 
-    def spyOn(self, player):
-        topcard = player.deck[-1]
+    def spyOn(self, attacker, victim):
+        c = victim.nextCard()
+        vicname = "your" if attacker == victim else "%s's" % victim.name
         options = [
-            {'selector': '0', 'print': "Don't Discard card", 'discard': False},
-            {'selector': '1', 'print': "Discard card", 'discard': True}
+            {'selector': '0', 'print': "Keep %s on deck" % c.name, 'discard': False},
+            {'selector': '1', 'print': "Discard %s" % c.name, 'discard': True}
             ]
-        o = player.userInput(options, "Discard %s's %s?" % (player.name, topcard.cardname))
+        o = attacker.userInput(options, "Discard %s card?" % vicname)
         if o['discard']:
-            player.deck.remove(topcard)
-            player.addCard(topcard)
+            victim.addCard(c, 'discard')
+        else:
+            victim.addCard(c, 'deck')
+
+
+###############################################################################
+class Test_Spy(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=2, initcards=['spy', 'moat'])
+        self.attacker = self.g.players[0]
+        self.defender = self.g.players[1]
+        self.attacker.setDeck('estate', 'province', 'duchy')
+        self.defender.setDeck('estate', 'gold')
+
+    def test_moat(self):
+        self.defender.setHand('moat')
+        scard = self.attacker.gainCard('spy', 'hand')
+        self.attacker.test_input = ['0']
+        self.attacker.playCard(scard)
+        self.assertEquals(self.attacker.deck[-1].name, 'Province')
+        self.assertEquals(self.defender.deck[-1].name, 'Gold')
+
+    def test_undefended(self):
+        scard = self.attacker.gainCard('spy', 'hand')
+        self.attacker.test_input = ['0', '0']
+        self.attacker.playCard(scard)
+        self.assertEquals(self.attacker.deck[-1].name, 'Province')
+        self.assertEquals(self.defender.deck[-1].name, 'Gold')
+
+    def test_discards(self):
+        scard = self.attacker.gainCard('spy', 'hand')
+        self.attacker.test_input = ['1', '1']
+        self.attacker.playCard(scard)
+        self.assertEquals(self.attacker.deck[-1].name, 'Estate')
+        self.assertEquals(self.defender.deck[-1].name, 'Estate')
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
