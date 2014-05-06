8aac8e8307112f413e9340d032f40e4d6329dbca
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-06 22:00:59 +1000

    Added extra prosperity base cards

--- src/cards/BaseCard_Colony.py
+++ src/cards/BaseCard_Colony.py
@@ -5,6 +5,7 @@ class Card_Colony(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'victory'
+        self.base = 'prosperity'
         self.desc = "+10 VP"
         self.basecard = True
         self.name = 'Colony'
