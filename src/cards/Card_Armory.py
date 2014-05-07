101465c8c896916dabb42f224f2ae5cb390f4d3b
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-07 21:23:52 +1000

    Added dark ages

---
+++ src/cards/Card_Armory.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+class Card_Armory(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.base = 'darkages'
+        self.desc = "Gain a card costing up to 4 putting it on top of your deck"
+        self.name = 'Armory'
+        self.cost = 4
+
+    def special(self, game, player):
+        """ Gain a card costing up to 4"""
+        c = player.plrGainCard(4, destination='deck')
+
+
+###############################################################################
+class Test_Armory(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['armory', 'feast'])
+        self.plr = self.g.players[0]
+        self.armory = self.g['armory'].remove()
+        self.plr.addCard(self.armory, 'hand')
+
+    def test_gainzero(self):
+        self.plr.test_input = ['0']
+        self.plr.playCard(self.armory)
+        self.assertEquals(len(self.plr.hand), 5)
+        self.assertEquals(self.plr.discardpile, [])
+
+    def test_gainone(self):
+        self.plr.test_input = ['1']
+        self.plr.deck = []
+        self.plr.playCard(self.armory)
+        self.assertEquals(len(self.plr.hand), 5)
+        self.assertEquals(self.plr.discardpile, [])
+        self.assertLessEqual(self.plr.deck[-1].cost, 4)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
+
+#EOF
