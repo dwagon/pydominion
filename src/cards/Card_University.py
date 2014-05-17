a2e486a7c291608b04d827bdd1e624ca344b5467
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-17 17:38:50 +1200

    Testing University

--- src/cards/Card_University.py
+++ src/cards/Card_University.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -13,6 +16,36 @@ class Card_University(Card):
 
     def special(self, game, player):
         """ Gain an action card costing up to 5"""
-        player.plrGainCard(5, actiononly=True)
+        c = player.plrGainCard(5, types={'action': True})
+        if c:
+            player.output("Gained %s from university" % c.name)
+
+
+###############################################################################
+class Test_University(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['university'])
+        self.plr = self.g.players[0]
+        self.university = self.g['university'].remove()
+        self.plr.addCard(self.university, 'hand')
+
+    def test_gain(self):
+        self.plr.test_input = ['1']
+        self.plr.playCard(self.university)
+        self.assertEqual(len(self.plr.discardpile), 1)
+        self.assertTrue(self.plr.discardpile[0].isAction())
+        self.assertLessEqual(self.plr.discardpile[0].cost, 5)
+
+    def test_none(self):
+        self.plr.test_input = ['0']
+        self.plr.playCard(self.university)
+        self.assertEqual(self.plr.discardpile, [])
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
