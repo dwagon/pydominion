657e329f2b86c4fb290e8d28c0923e05b280136f
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-18 18:00:57 +1200

    Tested (and fixed) countinghouse

--- src/cards/Card_Countinghouse.py
+++ src/cards/Card_Countinghouse.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -5,6 +8,7 @@ class Card_Countinghouse(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'action'
+        self.base = 'prosperity'
         self.desc = "Pull coppers out of discard"
         self.name = 'Counting House'
         self.cost = 5
@@ -16,7 +20,34 @@ class Card_Countinghouse(Card):
         for c in player.discardpile:
             if c.cardname == 'copper':
                 player.addCard(c, 'hand')
+                player.discardpile.remove(c)
                 count += 1
         player.output("Picked up %d coppers" % count)
+
+
+###############################################################################
+class Test_Countinghouse(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['countinghouse'])
+        self.plr = self.g.players[0]
+        self.ch = self.g['countinghouse'].remove()
+        self.plr.setHand()
+        self.plr.addCard(self.ch, 'hand')
+
+    def test_pullcoppers(self):
+        self.plr.setDiscard('copper', 'gold', 'duchy', 'copper')
+        self.plr.playCard(self.ch)
+        self.assertEqual(len(self.plr.hand), 2)
+        for c in self.plr.hand:
+            self.assertEqual(c.name, 'Copper')
+        for c in self.plr.discardpile:
+            self.assertNotEqual(c.name, 'Copper')
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
