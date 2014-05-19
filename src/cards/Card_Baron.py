45a87cb505495a48ee1b504a9d8d0e906fde0c41
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-19 16:56:16 +1200

    Tested and fixed baron

--- src/cards/Card_Baron.py
+++ src/cards/Card_Baron.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -14,20 +17,66 @@ class Card_Baron(Card):
     def special(self, game, player):
         """ You may discard an Estate card. If you do +4 GP. Otherwise,
             gain an estate card """
-
         hasEstate = player.inHand('Estate')
-
         if hasEstate:
             options = [
-                {'selector': '0', 'print': "Don't trash an Estate - Gain Estate", 'trash': False},
-                {'selector': '1', 'print': "Trash an Estate - Gain +4 Gold", 'trash': True}]
-            o = player.userInput(options, "Trash Estate")
-            if o['trash']:
-                player.output("Trashed %s" % hasEstate.cardname)
-                player.trashCard(hasEstate)
+                {'selector': '0', 'print': "Keep Estate - Gain another", 'discard': False},
+                {'selector': '1', 'print': "Discard an Estate - Gain +4 Gold", 'discard': True}]
+            o = player.userInput(options, "Discard Estate?")
+            if o['discard']:
+                player.discardCard(hasEstate)
                 player.t['gold'] += 4
                 return
-        print("Gained an Estate")
-        player.addCard(game['Estate'].remove())
+        player.output("Gained an Estate")
+        player.gainCard('estate')
+
+
+###############################################################################
+class Test_Baron(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['baron'])
+        self.plr = self.g.players[0]
+        self.baron = self.g['baron'].remove()
+
+    def test_play(self):
+        self.plr.addCard(self.baron, 'hand')
+        self.plr.test_input = ['0']
+        self.plr.playCard(self.baron)
+        self.assertEqual(self.plr.t['buys'], 2)
+
+    def test_noestate(self):
+        self.plr.setHand('copper', 'copper', 'copper')
+        self.plr.addCard(self.baron, 'hand')
+        self.plr.playCard(self.baron)
+        self.assertEqual(self.plr.t['gold'], 0)
+        self.assertEqual(self.plr.discardpile[0].name, 'Estate')
+        self.assertEqual(len(self.plr.discardpile), 1)
+
+    def test_discardestate(self):
+        self.plr.setHand('gold', 'estate', 'copper')
+        self.plr.addCard(self.baron, 'hand')
+        self.plr.test_input = ['1']
+        self.plr.playCard(self.baron)
+        self.assertEqual(self.plr.t['gold'], 4)
+        self.assertEqual(self.plr.discardpile[0].name, 'Estate')
+        self.assertEqual(len(self.plr.discardpile), 1)
+        self.assertEqual(self.plr.inHand('Estate'), None)
+
+    def test_keepestate(self):
+        self.plr.setHand('estate', 'gold', 'copper')
+        self.plr.addCard(self.baron, 'hand')
+        self.plr.test_input = ['0']
+        self.plr.playCard(self.baron)
+        self.assertEqual(self.plr.t['gold'], 0)
+        self.assertEqual(self.plr.discardpile[0].name, 'Estate')
+        self.assertEqual(len(self.plr.discardpile), 1)
+        self.assertNotEqual(self.plr.inHand('Estate'), None)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
