62ebc7062d69528803f5e5915a2f8fbe2d9504d8
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-21 17:25:26 +1200

    Tested and fixed city

--- src/cards/Card_City.py
+++ src/cards/Card_City.py
@@ -1,24 +1,76 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_City(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'action'
+        self.base = 'prosperity'
         self.desc = "+1 card, +2 action, more if stacks empty"
         self.name = 'City'
         self.cost = 5
         self.cards = 1
         self.actions = 2
 
+    ###########################################################################
     def special(self, game, player):
         """ If there are one or more empty Supply piles, +1 card.
         If there are two or more, +1 gold, +1 buy """
         empties = sum([1 for st in game.cardpiles if game[st].isEmpty()])
         if empties >= 1:
-            player.t['cards'] += 1
+            player.pickupCard()
         if empties >= 2:
             player.t['gold'] += 1
             player.t['buys'] += 1
+
+
+###############################################################################
+class Test_City(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['city', 'moat', 'cellar'])
+        self.plr = self.g.players[0]
+        self.city = self.g['city'].remove()
+        self.plr.addCard(self.city, 'hand')
+
+    def test_nostacks(self):
+        self.plr.playCard(self.city)
+        self.assertEqual(self.plr.t['actions'], 2)
+        self.assertEqual(len(self.plr.hand), 6)
+
+    def test_onestack(self):
+        while(True):
+            c = self.g['moat'].remove()
+            if not c:
+                break
+        self.plr.playCard(self.city)
+        self.assertEqual(self.plr.t['actions'], 2)
+        self.assertEqual(len(self.plr.hand), 7)
+
+    def test_twostack(self):
+        while(True):
+            c = self.g['moat'].remove()
+            if not c:
+                break
+        while(True):
+            c = self.g['cellar'].remove()
+            if not c:
+                break
+        self.plr.playCard(self.city)
+        self.assertEqual(self.plr.t['actions'], 2)
+        self.assertEqual(self.plr.t['gold'], 1)
+        # 1 default + 1 for city
+        self.assertEqual(self.plr.t['buys'], 2)
+        # 5 for hand, 1 for city, 1 for one stack
+        self.assertEqual(len(self.plr.hand), 7)
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
