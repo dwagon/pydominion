7da383caa83dd0c7006b9d3f5e5f77ee990256fe
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-26 16:33:49 +1200

    Tested border village

--- src/cards/Card_Bordervillage.py
+++ src/cards/Card_Bordervillage.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Bordervillage(Card):
     def __init__(self):
         Card.__init__(self)
@@ -11,10 +15,36 @@ class Card_Bordervillage(Card):
         self.cards = 1
         self.actions = 2
 
-    def hook_purchasedCard(self, game, player):
+    def hook_gainThisCard(self, game, player):
         """ When you gain this, gain a card costing less than this"""
         newcost = self.cost - 1
         player.output("Gain a card costing %d due to Border Village" % newcost)
         player.plrGainCard(cost=newcost)
+
+
+###############################################################################
+class Test_Bordervillage(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['bordervillage'])
+        self.plr = self.g.players[0]
+        self.bv = self.g['bordervillage'].remove()
+        self.plr.addCard(self.bv, 'hand')
+
+    def test_play(self):
+        self.plr.playCard(self.bv)
+        self.assertEqual(self.plr.t['actions'], 2)
+        self.assertEqual(len(self.plr.hand), 6)
+
+    def test_gain(self):
+        self.plr.test_input = ['4']
+        self.plr.gainCard('bordervillage')
+        self.assertEqual(len(self.plr.discardpile), 2)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
