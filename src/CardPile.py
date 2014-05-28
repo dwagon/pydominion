b13dedad9ea66d64b8a89238cd488ffc4b6d8ced
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-28 17:14:54 +1200

    Added loadClass() to prevent code duplication

--- src/CardPile.py
+++ src/CardPile.py
@@ -1,15 +1,21 @@
 import imp
 
 
+###############################################################################
 class CardPile(object):
     def __init__(self, cardname, numcards=10, cardpath='cards'):
         self.cardpath = cardpath
-        cardmodule = self.importCard(cardname=cardname)
-        self.cardclass = getattr(cardmodule, "Card_%s" % cardname)
-        self.card = self.cardclass()
         self.cardname = cardname
         self.numcards = numcards
+        self.card = self.loadClass(cardname)()
 
+    ###########################################################################
+    def loadClass(self, cardname, cardfile=None):
+        cardmodule = self.importCard(cardname=cardname, cardfile=cardfile)
+        self.cardclass = getattr(cardmodule, "Card_%s" % cardname)
+        return self.cardclass
+
+    ###########################################################################
     def importCard(self, cardname=None, cardfile=None):
         if cardfile:
             fp, pathname, desc = imp.find_module(cardfile, [self.cardpath, 'cards'])
@@ -21,12 +27,15 @@ class CardPile(object):
         cardmodule = imp.load_module(cardname, fp, pathname, desc)
         return cardmodule
 
+    ###########################################################################
     def __getattr__(self, name):
         return getattr(self.card, name)
 
+    ###########################################################################
     def isEmpty(self):
         return self.numcards == 0
 
+    ###########################################################################
     def remove(self):
         if self.numcards:
             self.numcards -= 1
@@ -34,9 +43,11 @@ class CardPile(object):
         else:
             return None
 
+    ###########################################################################
     def add(self):
         self.numcards += 1
 
+    ###########################################################################
     def __repr__(self):
         return "CardPile %s: %d" % (self.name, self.numcards)
 
