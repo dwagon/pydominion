6b5c01ad3b35203eae7567163daaa627c87a4f1c
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-30 17:35:43 +1000

    Tested Laboratory

--- src/cards/Card_Laboratory.py
+++ src/cards/Card_Laboratory.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Laboratory(Card):
     def __init__(self):
         Card.__init__(self)
@@ -11,5 +15,28 @@ class Card_Laboratory(Card):
         self.cards = 2
         self.actions = 1
         self.cost = 5
+
+
+###############################################################################
+class Test_Laboratory(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['laboratory'])
+        self.plr = self.g.players[0]
+        self.card = self.g['laboratory'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        """ Play a Laboratory """
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.t['actions'], 1)
+        # 5 hand, +2 for playing lab
+        self.assertEqual(len(self.plr.hand), 7)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
