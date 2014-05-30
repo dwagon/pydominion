d61995cbc38e0d66e3adeb73d3244de112ff0ea0
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-30 17:24:08 +1000

    Testing Gardens

--- src/cards/Card_Gardens.py
+++ src/cards/Card_Gardens.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -18,5 +21,33 @@ class Card_Gardens(Card):
         """ Worth 1VP for every 10 cards in your deck rounded down """
         numcards = len(player.discardpile + player.hand + player.deck)
         return int(numcards / 10)
+
+
+###############################################################################
+class Test_Gardens(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['gardens'])
+        self.plr = self.g.players[0]
+
+    def test_score_0(self):
+        self.plr.setHand('gardens', 'copper', 'copper')
+        self.plr.setDeck('copper', 'copper', 'copper')
+        self.plr.setDiscard('copper', 'copper', 'copper')
+        score = self.plr.getScoreDetails()
+        self.assertEqual(score['Gardens'], 0)
+
+    def test_score_1(self):
+        self.plr.setHand('gardens', 'copper', 'copper')
+        self.plr.setDeck('copper', 'copper', 'copper', 'copper')
+        self.plr.setDiscard('copper', 'copper', 'copper')
+        score = self.plr.getScoreDetails()
+        self.assertEqual(score['Gardens'], 1)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
