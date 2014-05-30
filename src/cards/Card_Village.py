f8c3e5a3c2289140c94b24a653503188ff40515c
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-30 18:45:30 +1000

    Tested Village

--- src/cards/Card_Village.py
+++ src/cards/Card_Village.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -12,4 +15,25 @@ class Card_Village(Card):
         self.actions = 2
         self.cost = 3
 
+
+###############################################################################
+class Test_Village(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['village'])
+        self.plr = self.g.players[0]
+        self.card = self.g['village'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        """ Play the village """
+        self.plr.playCard(self.card)
+        self.assertEqual(len(self.plr.hand), 6)
+        self.assertEqual(self.plr.t['actions'], 2)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 #EOF
