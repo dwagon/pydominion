6cf64feef01cee0fddc0844f1ee83a781686e10b
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-30 17:16:03 +1000

    Tested Festival

--- src/cards/Card_Festival.py
+++ src/cards/Card_Festival.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -12,5 +15,27 @@ class Card_Festival(Card):
         self.buys = 1
         self.gold = 2
         self.cost = 5
+
+
+###############################################################################
+class Test_Festival(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['festival'])
+        self.plr = self.g.players[0]
+        self.card = self.g['festival'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.t['actions'], 2)
+        self.assertEqual(self.plr.t['buys'], 2)
+        self.assertEqual(self.plr.t['gold'], 2)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
