443bb3d44842c3ed2eaed0036ea1e0bb1d437197
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-30 18:08:47 +1000

    Tested Moat

--- src/cards/Card_Moat.py
+++ src/cards/Card_Moat.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -11,5 +14,25 @@ class Card_Moat(Card):
         self.defense = True
         self.cost = 2
         self.cards = 2
+
+
+###############################################################################
+class Test_Library(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['moat'])
+        self.plr = self.g.players[0]
+        self.card = self.g['moat'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        """ Play a moat """
+        self.plr.playCard(self.card)
+        self.assertEqual(len(self.plr.hand), 7)
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
