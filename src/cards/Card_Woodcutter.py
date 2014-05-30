3a9f104e2891b7b74570ded4a956d93215202f29
Dougal Scott <Dougal.Scott@gmail.com>
2014-05-30 18:48:04 +1000

    Tested woodcutter

--- src/cards/Card_Woodcutter.py
+++ src/cards/Card_Woodcutter.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
@@ -11,5 +14,27 @@ class Card_Woodcutter(Card):
         self.buys = 1
         self.gold = 2
         self.cost = 3
+
+
+###############################################################################
+class Test_Woodcutter(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['woodcutter'])
+        self.plr = self.g.players[0]
+        self.card = self.g['woodcutter'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        """ Play the woodcutter """
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.t['gold'], 2)
+        self.assertEqual(self.plr.t['buys'], 2)
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
 
 #EOF
