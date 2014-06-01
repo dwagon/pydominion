5c50601e4f708cc735e417302d57439761119a1d
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-01 18:56:52 +1000

    Tested Colony

--- src/cards/BaseCard_Colony.py
+++ src/cards/BaseCard_Colony.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Colony(Card):
     def __init__(self):
         Card.__init__(self)
@@ -13,5 +17,24 @@ class Card_Colony(Card):
         self.cost = 11
         self.victory = 10
 
+
+###############################################################################
+class Test_Colony(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, prosperity=True)
+        self.g.startGame(numplayers=1)
+        self.plr = self.g.players[0]
+        self.card = self.g['colony'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_score(self):
+        """ Score a colony """
+        sc = self.plr.getScoreDetails()
+        self.assertEqual(sc['Colony'], 10)
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
 
 #EOF
