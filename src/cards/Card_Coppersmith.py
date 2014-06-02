8c841d4a381a1339aa3e1bc57db2281682d87e8c
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-03 08:33:59 +1000

    Tested coppersmith

--- src/cards/Card_Coppersmith.py
+++ src/cards/Card_Coppersmith.py
@@ -1,20 +1,55 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Coppersmith(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'action'
         self.base = 'intrigue'
-        self.desc = "Copper produces and extra +1 this turn"
+        self.desc = "Copper produces an extra +1 this turn"
         self.name = 'Coppersmith'
         self.cost = 4
 
     def hook_spendValue(self, game, player, card):
         """ Copper produces an extra 1 this turn """
         if card.name == 'Copper':
-            print "Copper worth 1 more"
+            player.output("Copper worth 1 more")
             return 1
         return 0
+
+
+###############################################################################
+class Test_Coppersmith(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['coppersmith'])
+        self.plr = self.g.players[0]
+        self.card = self.g['coppersmith'].remove()
+
+    def test_copper(self):
+        """ Copper should be worth two """
+        self.plr.setHand('copper')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+        self.plr.playCard(self.plr.hand[0])
+        self.assertEqual(self.plr.t['gold'], 2)
+
+    def test_silver(self):
+        """ Silver should be unchanged and worth two """
+        self.plr.setHand('silver')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+        self.plr.playCard(self.plr.hand[0])
+        self.assertEqual(self.plr.t['gold'], 2)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
 
 #EOF
