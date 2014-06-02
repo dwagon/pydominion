a29425dce44cedb5f94172727923c1d2277059a1
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-03 08:10:11 +1000

    Use plrTrashCard() to replace all of special

--- src/cards/Card_Chapel.py
+++ src/cards/Card_Chapel.py
@@ -15,31 +15,8 @@ class Card_Chapel(Card):
 
     def special(self, game, player):
         """ Trash up to 4 cards from your hand """
-        trash = []
         player.output("Trash up to four cards")
-        while(1):
-            options = [{'selector': '0', 'print': 'Finish trashing', 'card': None}]
-            index = 1
-            for c in player.hand:
-                sel = "%d" % index
-                trashtag = 'Untrash' if c in trash else 'Trash'
-                pr = "%s %s" % (trashtag, c.name)
-                options.append({'selector': sel, 'print': pr, 'card': c})
-                index += 1
-            o = player.userInput(options, "Trash which card?")
-            if not o['card']:
-                break
-            if o['card'] in trash:
-                trash.remove(o['card'])
-            else:
-                if len(trash) < 4:
-                    trash.append(o['card'])
-                else:
-                    player.output("Can only trash four cards")
-
-        for t in trash:
-            player.output("Trashing %s" % t.name)
-            player.trashCard(t)
+        player.plrTrashCard(num=4)
 
 
 ###############################################################################
