da9b66163ecccf23d7a7bf98f93eddede550ce7e
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-10 20:02:08 +1000

    Tested hoard

--- src/cards/Card_Hoard.py
+++ src/cards/Card_Hoard.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Hoard(Card):
     def __init__(self):
         Card.__init__(self)
@@ -16,5 +20,42 @@ class Card_Hoard(Card):
         if card.isVictory():
             player.output("Gaining Gold from Hoard")
             player.addCard(game['Gold'].remove())
+
+
+###############################################################################
+class Test_Hoard(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['hoard'])
+        self.plr = self.g.players[0]
+        self.card = self.g['hoard'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.t['gold'], 2)
+        self.assertEqual(self.plr.discardpile, [])
+
+    def test_buy_victory(self):
+        self.plr.playCard(self.card)
+        self.plr.buyCard('estate')
+        self.assertEqual(len(self.plr.discardpile), 2)
+        for c in self.plr.discardpile:
+            if c.name == 'Gold':
+                break
+        else:   # pragma: no cover
+            self.fail("Didn't pickup gold")
+
+    def test_buy_nonvictory(self):
+        self.plr.playCard(self.card)
+        self.plr.buyCard('copper')
+        self.assertEqual(len(self.plr.discardpile), 1)
+        self.assertEqual(self.plr.discardpile[-1].name, 'Copper')
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
 
 #EOF
