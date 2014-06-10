994fc52ac94a9d1786eb8798cee707055356563f
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-10 19:56:34 +1000

    Tested cutpurse

--- src/cards/Card_Cutpurse.py
+++ src/cards/Card_Cutpurse.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Cutpurse(Card):
     def __init__(self):
         Card.__init__(self)
@@ -19,5 +23,36 @@ class Card_Cutpurse(Card):
             if c:
                 player.output("%s discarded a copper" % victim.name)
                 victim.discardCard(c)
+            else:
+                player.output("%s had no coppers" % victim.name)
+
+
+###############################################################################
+class Test_Cutpurse(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=2, initcards=['cutpurse'])
+        self.plr = self.g.players[0]
+        self.victim = self.g.players[1]
+        self.card = self.g['cutpurse'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play_coppers(self):
+        self.victim.setHand('copper', 'copper', 'estate')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.victim.discardpile[-1].name, 'Copper')
+        self.assertEqual(len(self.victim.hand), 2)
+
+    def test_play_none(self):
+        self.victim.setHand('estate', 'estate', 'estate')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.victim.discardpile, [])
+        self.assertEqual(len(self.victim.hand), 3)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
 
 #EOF
