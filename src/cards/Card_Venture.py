d1b634f942c8938c27015a3d87af8110330e3f9a
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-16 18:41:15 +1000

    Tested and fixed venture

--- src/cards/Card_Venture.py
+++ src/cards/Card_Venture.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Venture(Card):
     def __init__(self):
         Card.__init__(self)
@@ -16,11 +20,55 @@ class Card_Venture(Card):
         while(1):
             c = player.pickupCard(verbose=False)
             if c.isTreasure():
-                player.output("Picked up %s" % c.name)
+                player.output("Picked up %s from Venture" % c.name)
+                player.playCard(c)
                 break
             else:
                 player.output("Picked up and discarded %s" % c.name)
                 player.t['gold'] -= c.gold    # Compensate for not keeping card
                 player.discardCard(c)
+
+
+###############################################################################
+class Test_Venture(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['venture'])
+        self.plr = self.g.players[0]
+        self.card = self.g['venture'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        """ Play a Venture """
+        self.plr.setDeck('gold')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.t['gold'], 3)  # Gold
+        for c in self.plr.played:
+            if c.name == 'Gold':
+                break
+        else:   # pragma: no cover
+            self.fail("Didn't play the gold")
+        self.assertEqual(self.plr.deck, [])
+
+    def test_discard(self):
+        """ Make sure we discard non-treasures """
+        self.plr.setDeck('gold', 'estate', 'estate')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.t['gold'], 3)  # Gold
+        for c in self.plr.played:
+            if c.name == 'Gold':
+                break
+        else:   # pragma: no cover
+            self.fail("Didn't play the gold")
+        self.assertEqual(len(self.plr.discardpile), 2)
+        for c in self.plr.discardpile:
+            if c.name != 'Estate':  # pragma: no cover
+                self.fail("Didn't discard the non-treasure")
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
 
 #EOF
