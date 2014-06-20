171bbe4908ab3bea3288a0e25cba2bd3093f062f
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-20 20:44:46 +1000

    Tested Platinum

--- src/cards/BaseCard_Platinum.py
+++ src/cards/BaseCard_Platinum.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Platinum(Card):
     def __init__(self):
         Card.__init__(self)
@@ -12,5 +16,26 @@ class Card_Platinum(Card):
         self.basecard = True
         self.gold = 5
         self.cost = 9
+
+
+###############################################################################
+class Test_Platinum(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, prosperity=True)
+        self.g.startGame(numplayers=1)
+        self.plr = self.g.players.values()[0]
+        self.card = self.g['platinum'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        """ Play a platinum """
+        self.plr.playCard(self.card)
+        self.assertEquals(self.plr.t['gold'], 5)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
 
 #EOF
