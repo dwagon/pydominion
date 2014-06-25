f62de75cd2e9b5df24300bfce98335e0b6e4cd10
Dougal Scott <Dougal.Scott@gmail.com>
2014-06-25 18:26:41 +1000

    Added Butcher

---
+++ src/cards/Card_Butcher.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Butcher(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.base = 'guilds'
+        self.desc = "+2 coins - trash a card to buy a card"
+        self.name = 'Butcher'
+        self.cost = 5
+
+    def special(self, game, player):
+        """ Take 2 Coin tokens. You may trash a card from your hand
+            and then pay any number of Coin tokens. If you did trash a
+            card, gain a card with a cost up to the cost of the trashed
+        card play the number of Coin tokens you paid """
+        player.gainCoins(2)
+        trash = player.plrChooseOptions(
+            'Trash a card to buy a card?',
+            ("Don't trash a card", False), ('Trash a card', True))
+        if not trash:
+            return
+        card = player.plrTrashCard(force=True)[0]
+        options = []
+        for i in range(player.getCoins() + 1):
+            sel = '%d' % i
+            options.append({'selector': sel, 'print': 'Add %d coins' % i, 'coins': i})
+        o = player.userInput(options, "Spend extra coins?")
+        cost = card.cost + o['coins']
+        player.trashCard(card)
+        player.coins -= o['coins']
+        player.plrGainCard(cost=cost)
+
+
+###############################################################################
+class Test_Butcher(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True)
+        self.g.startGame(numplayers=1, initcards=['butcher'])
+        self.plr = self.g.players.values()[0]
+        self.card = self.g['butcher'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        """ Play a butcher"""
+        self.plr.coins = 0
+        self.plr.test_input = ['0']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getCoins(), 2)
+
+    def test_trash_gold(self):
+        """ Trash a gold """
+        self.plr.setHand('gold', 'gold', 'gold')
+        self.plr.addCard(self.card, 'hand')
+        # Trash a card
+        # Trash card 3
+        # Spend 2 coin
+        # Buy card 1
+        self.plr.test_input = ['1', '3', '2', '1']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getCoins(), 0)
+        self.assertEqual(self.plr.handSize(), 2)
+        self.assertEqual(self.plr.discardSize(), 1)
+        for m in self.plr.messages:
+            if 'Province' in m:
+                break
+        else:   # pragma: no cover
+            self.fail("Couldn't get a province for 8")
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+#EOF
