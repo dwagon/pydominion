3d76119e470d65bcf7243242478d2fbb106631a0
Dougal Scott <Dougal.Scott@gmail.com>
2016-04-28 21:26:02 +1000

    Implemented Treasure Trove

---
+++ src/cards/Card_Treasure_Trove.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Treasure_Trove(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'treasure'
+        self.base = 'adventure'
+        self.desc = "+2 Coin. When you play this, gain a Gold and a Copper"
+        self.name = 'Treasure Trove'
+        self.coin = 2
+        self.cost = 5
+
+    def special(self, game, player):
+        """ When you play this, gain a Gold and a Copper """
+        player.gainCard('copper')
+        player.gainCard('gold')
+        player.output("Gained a Copper and a Gold")
+
+
+###############################################################################
+class Test_Treasure_Trove(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['treasure_trove'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['treasure_trove'].remove()
+
+    def test_play(self):
+        """ Play a treasure trove"""
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.discardpile[0].name, 'Copper')
+        self.assertEqual(self.plr.discardpile[1].name, 'Gold')
+        self.assertEqual(self.plr.discardSize(), 2)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
