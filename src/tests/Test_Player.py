2b52b5d1fd7c1b6071db4730f134a0284e2b12c2
Dougal Scott <Dougal.Scott@gmail.com>
2016-05-08 12:19:44 +1000

    More tests

--- src/tests/Test_Player.py
+++ src/tests/Test_Player.py
@@ -60,7 +60,7 @@ class Test_nextCard(unittest.TestCase):
         self.plr.deck.empty()
         self.plr.discardpile.empty()
         c = self.plr.nextCard()
-        self.assertEqual(c, None)
+        self.assertIsNone(c)
 
     def test_drawOne(self):
         self.plr.setDeck('province')
@@ -322,6 +322,115 @@ class Test_pickupCard(unittest.TestCase):
         self.assertEqual(self.plr.hand[0].name, 'Gold')
         self.assertEqual(self.plr.deckSize(), 0)
         self.assertEqual(self.plr.handSize(), 1)
+
+    def test_pick_nomore(self):
+        """ Test picking up a card when there isn't one to be had """
+        self.plr.setDeck()
+        self.plr.setDiscard()
+        self.plr.setHand()
+        c = self.plr.pickupCard()
+        self.assertIsNone(c)
+        self.assertEqual(self.plr.handSize(), 0)
+
+    def test_pickups(self):
+        """ Test pickupCards """
+        self.plr.setHand()
+        self.plr.pickupCards(3, verb='test')
+        self.assertEqual(self.plr.handSize(), 3)
+        count = 0
+        for msg in self.plr.messages:
+            if msg.startswith('test'):
+                count += 1
+        self.assertEqual(count, 3)
+
+
+###############################################################################
+class Test_misc(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['golem', 'witch'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+
+    def test_setPlayed(self):
+        self.plr.setPlayed('silver', 'copper')
+        self.assertEqual(self.plr.playedSize(), 2)
+
+    def test_setPlayed_empty(self):
+        self.plr.setPlayed()
+        self.assertEqual(self.plr.playedSize(), 0)
+
+    def test_setDiscard(self):
+        self.plr.setDiscard('silver', 'copper')
+        self.assertEqual(self.plr.discardSize(), 2)
+
+    def test_setHand(self):
+        self.plr.setHand('silver', 'copper')
+        self.assertEqual(self.plr.handSize(), 2)
+
+    def test_setDeck(self):
+        self.plr.setDeck('silver', 'copper')
+        self.assertEqual(self.plr.deckSize(), 2)
+
+    def test_coststr(self):
+        witch = self.g['witch'].remove()
+        golem = self.g['golem'].remove()
+        self.assertEqual(self.plr.coststr(witch), "3 coins")
+        self.assertEqual(self.plr.coststr(golem), "4 coins 1 potions")
+
+    def test_inHand(self):
+        silver = self.g['silver'].remove()
+        self.plr.setHand('silver')
+        self.assertFalse(self.plr.inHand('gold'))
+        self.assertTrue(self.plr.inHand('silver'))
+        self.assertTrue(self.plr.inHand(silver))
+
+    def test_getPotions(self):
+        self.plr.potions = 3
+        self.assertEqual(self.plr.getPotions(), 3)
+
+
+###############################################################################
+class Test_playableSelection(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['moat'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.moat = self.g['moat'].remove()
+
+    def test_play(self):
+        self.plr.addCard(self.moat, 'hand')
+        opts, ind = self.plr.playableSelection(1)
+        self.assertEqual(len(opts), 1)
+        self.assertEqual(opts[0]['selector'], 'b')
+        self.assertEqual(opts[0]['card'], self.moat)
+        self.assertEqual(ind, 2)
+
+
+###############################################################################
+class Test_spendableSelection(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['moat', 'alchemist'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.moat = self.g['moat'].remove()
+        self.potion = self.g['potion'].remove()
+
+    def test_play(self):
+        self.plr.setHand('copper', 'estate')
+        self.plr.addCard(self.potion, 'hand')
+        self.plr.addCard(self.moat, 'hand')
+        opts, ind = self.plr.spendableSelection(1)
+        self.assertEqual(len(opts), 3)
+        self.assertEqual(opts[0]['selector'], 'b')
+        self.assertEqual(opts[0]['action'], 'spendall')
+        self.assertTrue(opts[0]['print'].startswith('Spend all treasures'))
+        self.assertIsNone(opts[0]['card'])
+        self.assertEqual(opts[1]['selector'], 'c')
+        self.assertEqual(opts[1]['action'], 'spend')
+        self.assertTrue(opts[1]['print'].startswith('Spend Copper'))
+        self.assertEqual(opts[2]['selector'], 'd')
+        self.assertEqual(opts[2]['action'], 'spend')
+        self.assertEqual(ind, 4)
 
 
 ###############################################################################
