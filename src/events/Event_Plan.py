f4dcd113ba49bc8016faaae88e91c7ffd8e19fb5
Dougal Scott <Dougal.Scott@gmail.com>
2016-05-12 17:54:44 +0100

    Implement Plan event

--- src/events/Event_Plan.py
+++ src/events/Event_Plan.py
@@ -5,40 +5,40 @@ from Event import Event
 
 
 ###############################################################################
-class Event_Pathfinding(Event):
+class Event_Plan(Event):
     def __init__(self):
         Event.__init__(self)
         self.base = 'adventure'
-        self.desc = "Move your +1 Card Token to an Action Supply Pile"
-        self.name = "Pathfinding"
-        self.cost = 8
+        self.desc = "Move your Trashing token to an Action Supply pile"
+        self.name = "Plan"
+        self.cost = 3
 
     def special(self, game, player):
-        """ Move your +1 Card token to an Action Supply Pile """
+        """ Move your Trashing token to an Action Supply pile"""
         actionpiles = []
         for cp in game.cardpiles.values():
             if cp.isAction():
                 actionpiles.append(cp)
         stacks = player.cardSel(num=1, prompt='What stack to add the +1 Card Token to?', cardsrc=actionpiles)
         if stacks:
-            player.place_token('+Card', stacks[0])
+            player.place_token('Trashing', stacks[0])
 
 
 ###############################################################################
-class Test_Pathfinding(unittest.TestCase):
+class Test_Plan(unittest.TestCase):
     def setUp(self):
         import Game
-        self.g = Game.Game(quiet=True, numplayers=1, eventcards=['pathfinding'], initcards=['moat'])
+        self.g = Game.Game(quiet=True, numplayers=1, eventcards=['plan'], initcards=['moat'])
         self.g.startGame()
         self.plr = self.g.playerList()[0]
-        self.card = self.g.events['Pathfinding']
+        self.card = self.g.events['Plan']
 
-    def test_with_treasure(self):
-        """ Use Pathfinding """
-        self.plr.addCoin(8)
+    def test_play(self):
+        """ Perform a Plan """
+        self.plr.addCoin(3)
         self.plr.test_input = ['moat']
         self.plr.performEvent(self.card)
-        self.assertEqual(self.plr.tokens['+Card'], 'moat')
+        self.assertEqual(self.plr.tokens['Trashing'], 'moat')
         self.assertEqual(self.plr.getCoin(), 0)
 
 
