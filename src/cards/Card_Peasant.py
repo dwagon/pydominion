d5cec344ea2b38a66ff3c7604de297e2b78ba7db
Dougal Scott <Dougal.Scott@gmail.com>
2016-05-25 20:07:24 +0200

    Implemented Peasant

---
+++ src/cards/Card_Peasant.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Peasant(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = ['action', 'traveller']
+        self.base = 'adventure'
+        self.desc = "+1 Buy, +1 Coin; Discard to replace with Soldier"
+        self.name = 'Peasant'
+        self.traveller = True
+        self.buys = 1
+        self.coin = 1
+        self.cost = 2
+
+    def hook_discardCard(self, game, player):
+        """ Replace with Treasure Hunter """
+        player.replace_traveller(self, 'Soldier')
+
+
+###############################################################################
+class Test_Peasant(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Peasant'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Peasant'].remove()
+
+    def test_play(self):
+        """ Play a peasant """
+        self.plr.setHand()
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getBuys(), 2)
+        self.assertEqual(self.plr.getCoin(), 1)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
