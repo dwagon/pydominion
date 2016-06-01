f215510d6fc3998278a21007b49b492cdf0308cd
Dougal Scott <Dougal.Scott@gmail.com>
2016-06-01 18:10:28 +0100

    Implemented Storyteller

---
+++ src/cards/Card_Storyteller.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Storyteller(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.base = 'adventure'
+        self.desc = "+1 Action, +1 Coin; Play up to 3 Treasures from your hand. Pay all of your Coins; +1 Card per Coin paid"
+        self.name = 'Storyteller'
+        self.actions = 1
+        self.coin = 1
+        self.cost = 5
+
+    def special(self, game, player):
+        treasures = []
+        for card in player.hand:
+            if card.isTreasure():
+                treasures.append(card)
+        toplay = player.cardSel(
+            num=3,
+            cardsrc=treasures,
+            prompt="Play 3 treasures to convert coin to cards",
+            verbs=('Play', 'Unplay')
+            )
+        for card in toplay:
+            player.playCard(card)
+        player.output("Converting %d coin to cards" % player.coin)
+        player.pickupCards(player.coin)
+        player.coin = 0
+
+
+###############################################################################
+class Test_Storyteller(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Storyteller'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Storyteller'].remove()
+
+    def test_play(self):
+        """ Play a Storyteller """
+        self.plr.setHand('Copper', 'Copper', 'Silver', 'Gold')
+        self.plr.test_input = ['1', '2', 'silver', 'finish']
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+        self.assertEquals(self.plr.getActions(), 1)
+        # 1 from existing, 1 + story, 2 for two coppers and 2 for a silver
+        self.assertEquals(self.plr.handSize(), 1 + 1 + 2 + 2)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
