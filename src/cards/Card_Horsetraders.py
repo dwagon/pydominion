a39aefa85a8cd4ba0264763034cb973c09c5a3c7
Dougal Scott <Dougal.Scott@gmail.com>
2016-06-04 16:28:17 +1000

    Not done yet

---
+++ src/cards/Card_Horsetraders.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Horsetraders(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = ['action', 'reaction']
+        self.base = 'cornucopia'
+        self.desc = """+1 Buy. +3 Coins. Discard 2 cards.
+        When another player plays an Attack card, you may set this aside from your hand.
+        If you do, then at the start of your next turn, +1 Card and return this to your hand."""
+        self.name = 'Horse Traders'
+        self.buys = 1
+        self.coin = 3
+        self.cost = 4
+
+    def special(self, game, player):
+        player.plrDiscardCards(num=2)
+
+    def todo(self):     # TODO
+        """ When another player plays an Attack card, you may set
+        this aside from your hand.  If you do, then at the start
+        of your next turn, +1 Card and return this to your hand. """
+        pass
+
+
+###############################################################################
+class Test_Horsetraders(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Horse Traders'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Horse Traders'].remove()
+
+    def test_play(self):
+        self.plr.setHand('Estate', 'Duchy', 'Province')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.test_input = ['Estate', 'Duchy', 'Finish']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getBuys(), 2)
+        self.assertEqual(self.plr.getCoin(), 3)
+        self.assertEqual(self.plr.discardSize(), 2)
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
