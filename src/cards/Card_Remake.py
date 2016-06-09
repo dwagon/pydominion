9e87f426cf308d7d10a38b9eba89f2825346f9fb
Dougal Scott <Dougal.Scott@gmail.com>
2016-06-09 18:55:45 +1000

    Implemented Remake

---
+++ src/cards/Card_Remake.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from Card import Card
+import unittest
+
+
+###############################################################################
+class Card_Remake(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.base = 'cornucopia'
+        self.desc = "Do this twice: Trash a card from your hand, then gain a card costing exactly 1 more than the trashed card."
+        self.name = 'Remake'
+        self.cost = 4
+
+    def special(self, game, player):
+        for i in range(2):
+            player.output("Trash a card and gain one costing 1 more")
+            c = player.plrTrashCard()
+            if c:
+                player.plrGainCard(cost=c[0].cost+1, modifier='equal')
+
+
+###############################################################################
+class Test_Remake(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Remake', 'Moat'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Remake'].remove()
+
+    def test_playcard(self):
+        """ Play a remake """
+        self.plr.setHand('Copper', 'Estate')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.test_input = ['Estate', 'Silver', 'Copper', 'Finish']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.handSize(), 0)
+        self.assertIsNotNone(self.plr.inDiscard('Silver'))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+
+# EOF
