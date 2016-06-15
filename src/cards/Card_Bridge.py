0a6e44b825ffcc272f987a0f1aeb9e596c3d638d
Dougal Scott <Dougal.Scott@gmail.com>
2016-06-15 20:50:13 +1000

    Added tests for bridge

--- src/cards/Card_Bridge.py
+++ src/cards/Card_Bridge.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Bridge(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'action'
         self.base = 'intrigue'
-        self.desc = "+1 buy, +1 coin, all cards -1 cost"
+        self.desc = "+1 Buy +1 Coin. All cards (including cards in players’ hands) cost 1 less this turn, but not less than 0."
         self.name = 'Bridge'
         self.buys = 1
         self.coin = 1
@@ -18,4 +22,32 @@ class Card_Bridge(Card):
         if self in player.played:
             return -1
         return 0
+
+
+###############################################################################
+class Test_Bridge(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Bridge'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Bridge'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getBuys(), 2)
+        self.assertEqual(self.plr.getCoin(), 1)
+
+    def test_costreduction(self):
+        self.coin = 1
+        self.assertEqual(self.plr.cardCost(self.g['Gold']), 6)
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.cardCost(self.g['Gold']), 5)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
 # EOF
