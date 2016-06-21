a1ed1c2df33b2038343aa8ca6dc96e8e6211b368
Dougal Scott <Dougal.Scott@gmail.com>
2016-06-22 08:46:04 +1000

    Implemented Soothsayer

---
+++ src/cards/Card_Soothsayer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Soothsayer(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = ['action', 'attack']
+        self.base = 'guilds'
+        self.desc = "Gain a Gold. Each other player gains a Curse. Each player who did draws a card."
+        self.needcurse = True
+        self.name = 'Soothsayer'
+        self.cost = 5
+
+    def special(self, game, player):
+        player.output("Gained up a Gold")
+        player.gainCard('Gold')
+        for pl in player.attackVictims():
+            player.output("%s got cursed" % pl.name)
+            pl.output("%s's Soothsayer cursed you" % player.name)
+            pl.gainCard('Curse')
+            pl.pickupCard()
+
+
+###############################################################################
+class Test_Soothsayer(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=2, initcards=['Soothsayer'])
+        self.g.startGame()
+        self.attacker, self.victim = self.g.playerList()
+        self.wcard = self.g['Soothsayer'].remove()
+        self.attacker.addCard(self.wcard, 'hand')
+
+    def test_play(self):
+        self.attacker.playCard(self.wcard)
+        self.assertEqual(self.victim.handSize(), 6)
+        self.assertIsNotNone(self.victim.inDiscard('Curse'))
+        self.assertIsNotNone(self.attacker.inDiscard('Gold'))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
