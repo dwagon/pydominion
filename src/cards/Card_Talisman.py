f4d47f968d861d5e578b409d69e26c172822cefd
Dougal Scott <Dougal.Scott@gmail.com>
2016-06-26 16:32:41 +1000

    Fix a duchess induced test failure

--- src/cards/Card_Talisman.py
+++ src/cards/Card_Talisman.py
@@ -27,7 +27,7 @@ class Card_Talisman(Card):
 class Test_Talisman(unittest.TestCase):
     def setUp(self):
         import Game
-        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Talisman'])
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Talisman'], badcards=['Duchess'])
         self.g.startGame()
         self.plr = self.g.playerList(0)
         self.card = self.g['Talisman'].remove()
