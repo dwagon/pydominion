d96a58b3ae743c6c35d08b048019fc1a27a47ef3
Dougal Scott <Dougal.Scott@gmail.com>
2016-06-26 17:11:15 +1000

    Fix watchtower description

--- src/cards/Card_Watchtower.py
+++ src/cards/Card_Watchtower.py
@@ -9,7 +9,8 @@ class Card_Watchtower(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = ['action', 'reaction']
-        self.desc = "Draw up to 6 cards. Can trash gained cards or put on top of deck"
+        self.desc = """Draw until you have 6 cards in hand.
+        When you gain a card, you may reveal this from your hand. If you do, either trash that card, or put it on top of your deck."""
         self.name = 'Watchtower'
         self.cost = 3
 
