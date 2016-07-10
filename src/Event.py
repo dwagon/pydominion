fe41575c5eb9b76bfa7b5613fff17a14d845f8c1
Dougal Scott <Dougal.Scott@gmail.com>
2016-07-10 11:24:03 +1000

    Handle debtcost

--- src/Event.py
+++ src/Event.py
@@ -2,6 +2,7 @@ class Event(object):
     def __init__(self):
         self.image = None
         self.cost = 0
+        self.debtcost = 0
         self.desc = 'TODO'
 
     def hook_endTurn(self, game, player):   # pragma: no cover
