e783dd80ce1dcb1c311616a27a8e1ce2d7e35247
Dougal Scott <Dougal.Scott@gmail.com>
2016-07-10 13:38:29 +1000

    Implement Dominate

---
+++ src/events/Event_Dominate.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import unittest
+from Event import Event
+
+
+###############################################################################
+class Event_Dominate(Event):
+    def __init__(self):
+        Event.__init__(self)
+        self.base = 'empires'
+        self.desc = "Gain a Province. If you do, +9VP."
+        self.name = "Dominate"
+        self.cost = 14
+
+    def special(self, game, player):
+        c = player.gainCard('Province')
+        if c:
+            player.addScore('Dominate', 9)
+
+
+###############################################################################
+class Test_Dominate(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, eventcards=['Dominate'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g.events['Dominate']
+
+    def test_play(self):
+        """ Perform a Dominate """
+        self.plr.addCoin(14)
+        self.plr.performEvent(self.card)
+        self.assertIsNotNone(self.plr.inDiscard('Province'))
+        self.assertEqual(self.plr.getScoreDetails()['Dominate'], 9)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
