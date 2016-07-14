0ff4d42f2b20240f3edd3f0a22d863831ad0cbed
Dougal Scott <Dougal.Scott@gmail.com>
2016-07-14 21:14:33 +1000

    Implemented Small Castle

---
+++ src/cards/Castle_Small.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import unittest
+from cards.Card_Castles import CastleCard
+
+
+###############################################################################
+class Card_SmallCastle(CastleCard):
+    def __init__(self):
+        CastleCard.__init__(self)
+        self.cardtype = ['action', 'victory', 'castle']
+        self.base = 'empires'
+        self.cost = 5
+        self.desc = "Trash this or a Castle from your hand. If you do, gain a Castle. 2VP"
+        self.coin = 1
+        self.name = "Small Castle"
+        self.victory = 2
+
+    def special(self, game, player):
+        cards = [c for c in player.hand if c.isCastle()] + [self]
+        tr = player.plrTrashCard(prompt="Trash a Castle to gain another Castle", cardsrc=cards)
+        if tr:
+            newcast = player.gainCard('Castles')
+            player.output("Gained %s" % newcast.name)
+
+
+###############################################################################
+class Test_SmallCastle(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Castles'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        while True:
+            self.card = self.g['Castles'].remove()
+            if self.card.name == 'Small Castle':
+                break
+
+    def test_play(self):
+        """ Play a castle - trash nothing"""
+        self.plr.test_input = ['Finish']
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getScoreDetails()['Small Castle'], 2)
+
+    def test_trash(self):
+        """ Play a castle - trash self"""
+        self.plr.test_input = ['small']
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
