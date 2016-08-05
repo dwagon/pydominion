5dcd3ce5a6b7017299684c1b20ad4ba4b8095855
Dougal Scott <Dougal.Scott@gmail.com>
2016-08-05 19:17:23 +1000

    Implemented Crown

--- src/cards/Card_Crown.py
+++ src/cards/Card_Crown.py
@@ -17,46 +17,65 @@ class Card_Crown(Card):
 
     def special(self, game, player):
         if player.phase == 'action':
-            options = [{'selector': '0', 'print': "Don't play a card", 'card': None}]
-            index = 1
-            for c in player.hand:
-                if not c.isAction():
-                    continue
-                sel = "%d" % index
-                pr = "Play %s twice" % c.name
-                options.append({'selector': sel, 'print': pr, 'card': c})
-                index += 1
-            if index == 1:
-                return
-            o = player.userInput(options, "Play which action card twice?")
-            if not o['card']:
-                return
-            for i in range(1, 3):
-                player.output("Number %d play of %s" % (i, o['card'].name))
-                player.playCard(o['card'], discard=False, costAction=False)
-            player.discardCard(o['card'])
+            cards = [c for c in player.hand if c.isAction()]
+            self.do_twice(player, cards)
+        if player.phase == 'buy':
+            cards = [c for c in player.hand if c.isTreasure()]
+            self.do_twice(player, cards)
+
+    def do_twice(self, player, cards):
+        if not cards:
+            player.output("No suitable cards")
+            return
+        options = [{'selector': '0', 'print': "Don't play a card", 'card': None}]
+        index = 1
+        for c in cards:
+            sel = "%d" % index
+            pr = "Play %s twice" % c.name
+            options.append({'selector': sel, 'print': pr, 'card': c})
+            index += 1
+        o = player.userInput(options, "Play which card twice?")
+        if not o['card']:
+            return
+        for i in range(1, 3):
+            player.output("Number %d play of %s" % (i, o['card'].name))
+            player.playCard(o['card'], discard=False, costAction=False)
+        player.discardCard(o['card'])
 
 
 ###############################################################################
 class Test_Crown(unittest.TestCase):
     def setUp(self):
         import Game
-        self.g = Game.Game(quiet=True, numplayers=2, initcards=['Crown'])
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Crown', 'Moat'])
         self.g.startGame()
-        self.plr, self.other = self.g.playerList()
+        self.plr = self.g.playerList(0)
         self.card = self.g['Crown'].remove()
 
     def test_play(self):
-        self.other.setHand('Copper', 'Silver', 'Gold')
-        self.plr.setHand('Duchy', 'Province', 'Gold', 'Silver', 'Estate')
+        """ Play a crown with no suitable actions """
+        self.plr.setHand('Duchy', 'Gold')
         self.plr.addCard(self.card, 'hand')
-        self.other.test_input = ['Copper', 'Silver', 'Finish']
-        self.plr.test_input = ['Duchy', 'Province', 'Finish']
+        self.plr.phase = 'action'
         self.plr.playCard(self.card)
-        self.assertEqual(self.plr.handSize(), 5 + 2 - 2)
-        self.assertEqual(self.plr.getCoin(), 2)
-        self.assertEqual(self.other.handSize(), 3 - 2 + 1)
 
+    def test_action(self):
+        """ Play a crown with a suitable action """
+        self.plr.setHand('Estate', 'Duchy', 'Copper', 'Gold', 'Moat')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.phase = 'action'
+        self.plr.test_input = ['moat']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.handSize(), 5 + 2 * 2 - 1)
+
+    def test_buy(self):
+        """ Play a crown in a buy phase"""
+        self.plr.setHand('Estate', 'Duchy', 'Copper', 'Gold')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.phase = 'buy'
+        self.plr.test_input = ['gold']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getCoin(), 3 * 2)
 
 ###############################################################################
 if __name__ == "__main__":  # pragma: no cover
