1eba0e51adc6db3f432025e788a485a7173affe7
Dougal Scott <Dougal.Scott@gmail.com>
2016-11-17 09:03:45 +1100

    Fix minor wording issue

--- src/events/Event_Training.py
+++ src/events/Event_Training.py
@@ -16,7 +16,7 @@ class Event_Training(Event):
     def special(self, game, player):
         """ Move your +1 Coin token to an Action Supply Pile """
         actionpiles = game.getActionPiles()
-        stacks = player.cardSel(num=1, prompt='What stack to add the +1 Card Token to?', cardsrc=actionpiles)
+        stacks = player.cardSel(num=1, prompt='What stack to add the +1 Coin Token to?', cardsrc=actionpiles)
         if stacks:
             player.place_token('+1 Coin', stacks[0].name)
 
