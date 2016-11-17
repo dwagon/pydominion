17f153c8e0e574e2329a338e5628501f0360f630
Dougal Scott <Dougal.Scott@gmail.com>
2016-11-17 19:04:21 +1100

    Mutable desc on BorderVillage

--- src/cards/Card_Bordervillage.py
+++ src/cards/Card_Bordervillage.py
@@ -9,11 +9,16 @@ class Card_Bordervillage(Card):
     def __init__(self):
         Card.__init__(self)
         self.cardtype = 'action'
-        self.desc = "+1 card, +2 action. When you gain this, gain a card costing less than this"
         self.name = 'Border Village'
         self.cost = 6
         self.cards = 1
         self.actions = 2
+
+    def desc(self, player):
+        if player.phase == "buy":
+            return "+1 card, +2 action. When you gain this, gain a card costing less than this"
+        else:
+            return "+1 card, +2 action"
 
     def hook_gainThisCard(self, game, player):
         """ When you gain this, gain a card costing less than this"""
