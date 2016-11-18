7bbae31cf74b8ccfb8d80c8304b8523393c1b265
Dougal Scott <dougal.scott@gmail.com>
2016-11-18 22:15:37 +1100

    Palace - fix score and output

--- src/landmarks/Landmark_Palace.py
+++ src/landmarks/Landmark_Palace.py
@@ -17,7 +17,8 @@ class Landmark_Palace(Landmark):
         for card in player.allCards():
             if card.name in num:
                 num[card.name] += 1
-        score = min(num.values())
+        score = min(num.values()) * 3
+        player.output("Palace scored %d VP (%d Copper, %d Silver, %d Gold)" % (score, num['Copper'], num['Silver'], num['Gold']))
         player.addScore('Palace', score)
 
 
@@ -35,7 +36,7 @@ class Test_Palace(unittest.TestCase):
         self.plr.setDeck('Gold', 'Silver', 'Copper', 'Copper', 'Duchy')
         self.plr.setHand('Silver', 'Copper', 'Copper', 'Copper', 'Duchy')
         self.plr.gameOver()
-        self.assertEqual(self.plr.getScoreDetails()['Palace'], 2)
+        self.assertEqual(self.plr.getScoreDetails()['Palace'], 2 * 3)
 
 
 ###############################################################################
