638ff7c0305cbbf6aa33a9f182970741cef3e26f
Dougal Scott <dougal.scott@gmail.com>
2016-12-07 18:45:13 +1100

    Fixed Desc

--- src/cards/Card_Count.py
+++ src/cards/Card_Count.py
@@ -10,7 +10,7 @@ class Card_Count(Card):
         Card.__init__(self)
         self.cardtype = 'action'
         self.base = 'darkages'
-        self.desc = "Lots of choices"
+        self.desc = """Choose one: Discard 2 cards; or put a card from your hand on top of your deck; or gain a Copper. Choose one: +3 Coin; or trash your hand; or gain a Duchy"""
         self.name = 'Count'
         self.cost = 5
 
