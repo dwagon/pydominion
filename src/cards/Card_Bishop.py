5870072f121f86eec76cd9e41eb2a5d05116980e
Dougal Scott <dougal.scott@gmail.com>
2016-12-09 18:07:45 +1100

    Fix desc

--- src/cards/Card_Bishop.py
+++ src/cards/Card_Bishop.py
@@ -10,7 +10,7 @@ class Card_Bishop(Card):
         Card.__init__(self)
         self.cardtype = 'action'
         self.base = 'prosperity'
-        self.desc = "Trash a card for VP"
+        self.desc = "+1 Coin, +1 VP; Trash a card from your hand. +VP equal to half its cost in coins, rounded down. Each other player may trash a card from his hand"
         self.name = 'Bishop'
         self.coin = 1
         self.victory = 1
