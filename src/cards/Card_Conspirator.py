643a4c7e18a0d0b70dd9148d3ca1218dff83a60e
Dougal Scott <dougal.scott@gmail.com>
2017-01-31 09:01:16 +1100

    Fixed desc

--- src/cards/Card_Conspirator.py
+++ src/cards/Card_Conspirator.py
@@ -10,14 +10,13 @@ class Card_Conspirator(Card):
         Card.__init__(self)
         self.cardtype = 'action'
         self.base = 'intrigue'
-        self.desc = "+2 coin. If played more than 3 actions +1 card, +1 action"
+        self.desc = """+2 coin. If you've player 3 or more actions this turn (counting
+            this); +1 card, +1 action """
         self.name = 'Conspirator'
         self.coin = 2
         self.cost = 4
 
     def special(self, player, game):
-        """ If you've player 3 or more actions this turn (counting
-            this); +1 card, +1 action """
         if self.numActionsPlayed(player) >= 3:
             player.pickupCard()
             player.addActions(1)
