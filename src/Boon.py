4c1b068205f05d1ae3ca27bf7699ec9676426cc5
Dougal Scott <dougal.scott@gmail.com>
2017-12-18 09:50:58 +1100

    Moved to card

--- src/Boon.py
+++ src/Boon.py
@@ -2,9 +2,7 @@ from Card import Card
 
 
 class Boon(Card):
-    def __init__(self, *args, **kwargs):
-        super(Card, self).__init__(*args, **kwargs)
-        self.retain_boon = False
+    pass
 
 
 # EOF
