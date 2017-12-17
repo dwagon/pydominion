400a4af63effbb8b46127ca51c756f434f0b88b7
Dougal Scott <dougal.scott@gmail.com>
2017-12-18 09:40:26 +1100

    Backwards compat with py2

--- src/Boon.py
+++ src/Boon.py
@@ -3,7 +3,7 @@ from Card import Card
 
 class Boon(Card):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(Card, self).__init__(*args, **kwargs)
         self.retain_boon = False
 
 
