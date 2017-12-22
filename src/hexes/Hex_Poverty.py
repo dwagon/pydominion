b655c1a9bf31f30ad1ee54bf06405f698a7ff674
Dougal Scott <dougal.scott@gmail.com>
2017-12-22 13:10:38 +1100

    Implemented Poverty

---
+++ src/hexes/Hex_Poverty.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import unittest
+from Hex import Hex
+
+
+###############################################################################
+class Hex_Poverty(Hex):
+    def __init__(self):
+        Hex.__init__(self)
+        self.cardtype = 'hex'
+        self.base = 'nocture'
+        self.desc = "Discard down to 3 cards in hand"
+        self.name = "Poverty"
+        self.purchasable = False
+
+    def special(self, game, player):
+        player.plrDiscardDownTo(3)
+
+
+###############################################################################
+class Test_Poverty(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Cursed Village'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        for h in self.g.hexes[:]:
+            if h.name != "Poverty":
+                self.g.discarded_hexes.append(h)
+                self.g.hexes.remove(h)
+
+    def test_normal(self):
+        self.plr.test_input = ['1', '2', '0']
+        self.plr.gainCard('Cursed Village')
+        self.assertEqual(self.plr.handSize(), 3)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
