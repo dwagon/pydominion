fb53d65a1009553be174937450f1acad3ffe3e34
Dougal Scott <dougal.scott@gmail.com>
2017-12-24 10:36:41 +1100

    Implemented Fear

---
+++ src/hexes/Hex_Fear.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import unittest
+from Hex import Hex
+
+
+###############################################################################
+class Hex_Fear(Hex):
+    def __init__(self):
+        Hex.__init__(self)
+        self.cardtype = 'hex'
+        self.base = 'nocture'
+        self.desc = "If you have at least 5 cards in hand, discard an Action or Treasure"
+        self.name = "Fear"
+        self.purchasable = False
+
+    def special(self, game, player):
+        if player.handSize() < 5:
+            return
+        tanda = [_ for _ in player.hand if _.isAction() or _.isTreasure()]
+        player.plrDiscardCards(num=1, cardsrc=tanda, prompt="Discard an Action or a Treasure")
+
+
+###############################################################################
+class Test_Fear(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Cursed Village'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        for h in self.g.hexes[:]:
+            if h.name != "Fear":
+                self.g.discarded_hexes.append(h)
+                self.g.hexes.remove(h)
+
+    def test_empty_war(self):
+        self.plr.setHand('Estate', 'Duchy', 'Province', 'Gold')
+        self.plr.gainCard('Cursed Village')
+        self.assertEqual(self.plr.discardSize(), 1)     # The Cursed Village
+
+    def test_war(self):
+        self.plr.setHand('Estate', 'Duchy', 'Estate', 'Duchy', 'Copper')
+        self.plr.test_input = ['Copper']
+        self.plr.gainCard('Cursed Village')
+        self.assertEqual(self.plr.discardSize(), 2)
+        self.assertIsNotNone(self.plr.inDiscard('Copper'))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
