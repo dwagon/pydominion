ba0f9be991d0bda696f9273ad2d2c247c71c3300
Dougal Scott <dougal.scott@gmail.com>
2017-12-23 11:42:42 +1100

    Implemented Devil's Workshop

---
+++ src/cards/Card_Devils_Workshop.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Devils_Workshop(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'night'
+        self.base = 'nocturne'
+        self.desc = "If the number of cards you've gained this turn is: 2+, gain an Imp from its pile; 1, gain a card costing up to 4; 0, gain a Gold."
+        self.name = "Devil's Workshop"
+        self.cost = 4
+        self.required_cards = [('Card', 'Imp')]
+
+    def special(self, game, player):
+        nc = len(player.stats['gained'])
+        if nc >= 2:
+            player.gainCard('Imp')
+        elif nc == 1:
+            player.plrGainCard(4)
+        else:
+            player.gainCard('Gold')
+
+
+###############################################################################
+class Test_Devils_Workshop(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=["Devil's Workshop"])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g["Devil's Workshop"].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play_0(self):
+        self.plr.playCard(self.card)
+        self.assertIsNotNone(self.plr.inDiscard('Gold'))
+
+    def test_play_1(self):
+        self.plr.gainCard('Copper')
+        self.plr.test_input = ['1']
+        self.plr.playCard(self.card)
+        self.assertLessEqual(self.plr.discardpile[0].cost, 4)
+
+    def test_play_2(self):
+        self.plr.gainCard('Copper')
+        self.plr.gainCard('Estate')
+        self.plr.playCard(self.card)
+        self.assertIsNotNone(self.plr.inDiscard('Imp'))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
