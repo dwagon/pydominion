952f64d925c41aef4d4cf084d81a7b3a05dbd62d
Dougal Scott <dougal.scott@gmail.com>
2017-12-27 12:39:32 +1100

    Bad test

--- src/cards/Card_Feast.py
+++ src/cards/Card_Feast.py
@@ -50,7 +50,7 @@ class Card_Feast(Card):
 class Test_Feast(unittest.TestCase):
     def setUp(self):
         import Game
-        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Feast'], badcards=['Den of Sin'])
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Feast'], badcards=['Den of Sin', 'Ghost Town'])
         self.g.startGame()
         self.plr = self.g.playerList(0)
         self.card = self.g['Feast'].remove()
