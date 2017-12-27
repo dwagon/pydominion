133a67042c5cb1e4ba24bddac9289ca77078a268
Dougal Scott <dougal.scott@gmail.com>
2017-12-27 12:18:44 +1100

    Bad trash test

--- src/cards/Card_Cultist.py
+++ src/cards/Card_Cultist.py
@@ -91,7 +91,7 @@ class Test_Cultist(unittest.TestCase):
         """ Trashing a cultist should give 3 more cards """
         self.plr.addCard(self.card, 'hand')
         self.plr.trashCard(self.card)
-        self.assertEqual(self.g.trashpile[0].name, 'Cultist')
+        self.assertIsNotNone(self.g.inTrash('Cultist'))
         self.assertEqual(self.plr.handSize(), 8)
 
 
