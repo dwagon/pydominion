948e5277da67c382df6ef3ee6d6900f27173af72
Dougal Scott <dougal.scott@gmail.com>
2017-12-28 16:23:54 +1100

    Stop duchess breaking test

--- src/cards/Card_Feast.py
+++ src/cards/Card_Feast.py
@@ -50,14 +50,14 @@ class Card_Feast(Card):
 class Test_Feast(unittest.TestCase):
     def setUp(self):
         import Game
-        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Feast'], badcards=['Den of Sin', 'Ghost Town'])
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Feast'], badcards=['Den of Sin', 'Ghost Town', 'Duchess'])
         self.g.startGame()
         self.plr = self.g.playerList(0)
         self.card = self.g['Feast'].remove()
         self.plr.addCard(self.card, 'hand')
 
     def test_dontTrash(self):
-        self.plr.test_input = ['keep']
+        self.plr.test_input = ['keep this']
         self.plr.playCard(self.card)
         self.assertTrue(self.g.trashpile.isEmpty())
         self.assertEqual(self.plr.played[0].name, 'Feast')
