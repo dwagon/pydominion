409fcdb6613edb42fb7af6c64f8d837c43f8338f
Dougal Scott <dougal.scott@gmail.com>
2018-01-03 10:52:00 +1100

    Implemented Enchantress

---
+++ src/cards/Card_Enchantress.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from Card import Card
+import unittest
+
+
+###############################################################################
+class Card_Enchantress(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = ['action', 'attack', 'duration']
+        self.base = 'empires'
+        self.desc = """Until your next turn, the first time each other player plays an
+            Action card on their turn, they get +1 Card and +1 Action instead of
+            following its instructions. At the start of your next turn, +2 Cards"""
+        self.name = "Enchantress"
+        self.cost = 3
+
+    def duration(self, game, player):
+        player.addCards(2)
+
+    def hook_allPlayers_preAction(self, game, player, owner, card):
+        if len(player.played) == 0:
+            player.output("{}'s Enchantress gazump'd your {}".format(owner.name, card.name))
+            player.addActions(1)
+            player.pickupCard()
+            return {'skip_card': True}
+
+
+###############################################################################
+class Test_Enchantress(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=2, initcards=["Enchantress", "Remodel", "Moat"])
+        self.g.startGame()
+        self.plr, self.vic = self.g.playerList()
+        self.card = self.g["Enchantress"].remove()
+        self.r1 = self.g["Remodel"].remove()
+        self.m1 = self.g["Moat"].remove()
+
+    def test_play(self):
+        """ Play an Enchantress"""
+        self.plr.addCard(self.card, 'hand')
+        self.plr.playCard(self.card)
+        self.vic.addCard(self.r1, 'hand')
+        self.vic.playCard(self.r1)
+        self.assertEqual(self.vic.handSize(), 5 + 1)    # Hand + Ench
+        self.assertEqual(self.vic.getActions(), 1)
+        self.vic.addCard(self.m1, 'hand')
+        self.vic.playCard(self.m1)
+        self.g.print_state()
+        self.assertEqual(self.vic.handSize(), 5 + 1 + 2)    # Hand + Ench + Moat
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
