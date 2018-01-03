1aaf1ece87aeb4b095c7d2e571906ad347a1e679
Dougal Scott <dougal.scott@gmail.com>
2018-01-03 12:55:15 +1100

    Implemented Secret Passage

---
+++ src/cards/Card_Secret_Passage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_SecretPassage(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.base = 'intrigue'
+        self.desc = """+2 Cards; +1 Action; Take a card from your hand and put it anywhere in your deck."""
+        self.name = 'Secret Passage'
+        self.cost = 4
+        self.actions = 1
+        self.cards = 2
+
+    def special(self, game, player):
+        card = player.cardSel(
+                prompt="Take a card from your hand and put into your deck",
+                cardsrc='hand'
+                )
+        if card:
+            dest = player.plrChooseOptions(
+                    "Put {} into top or bottom of deck".format(card[0].name),
+                    ("Top of deck", "topdeck"),
+                    ("Bottom of deck", "deck")
+                    )
+            player.addCard(card[0], dest)
+            player.hand.remove(card[0])
+
+
+###############################################################################
+class Test_SecretPassage(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Secret Passage', 'Moat'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Secret Passage'].remove()
+
+    def test_play(self):
+        """ Play an Secret Passage """
+        self.plr.setHand('Gold', 'Province', 'Duchy', 'Copper', 'Silver')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.test_input = ['Select Province', 'Bottom']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.getActions(), 1)
+        self.assertEqual(self.plr.handSize(), 5 + 2)
+        self.assertEqual(self.plr.deck[0].name, 'Province')
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
