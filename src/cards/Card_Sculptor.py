c6fa0cb4b5b0d8f6334f3805c86d3bc26c14aacc
Dougal Scott <dougal.scott@gmail.com>
2018-11-25 18:11:01 +1100

    Implemented Sculptor

---
+++ src/cards/Card_Sculptor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Sculptor(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.base = 'renaissance'
+        self.desc = """Gain a card to your hand costing up to 4. If it's a Treasure, +1 Villager."""
+        self.name = 'Sculptor'
+        self.cost = 5
+
+    ###########################################################################
+    def special(self, game, player):
+        card = player.plrGainCard(4, destination='hand', force=True)
+        if card.isTreasure():
+            player.output("Gained  villager")
+            player.gainVillager()
+
+
+###############################################################################
+class Test_Sculptor(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Sculptor', 'Moat'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Sculptor'].remove()
+        self.plr.setHand()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_gainaction(self):
+        self.plr.setDeck('Moat')
+        self.plr.test_input = ['Moat']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.discardSize(), 0)
+        self.assertIsNotNone(self.plr.inHand('Moat'))
+        self.assertLessEqual(self.plr.getVillager(), 1)
+
+    def test_gaintreasure(self):
+        self.plr.setDeck('Silver')
+        self.plr.test_input = ['Silver']
+        self.plr.playCard(self.card)
+        self.assertIsNotNone(self.plr.inHand('Silver'))
+        self.assertLessEqual(self.plr.getVillager(), 1)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
