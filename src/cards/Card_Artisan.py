9cbba97f66b9fde96b84d65cad372f0638566349
Dougal Scott <dougal.scott@gmail.com>
2019-05-07 14:44:05 +1000

    Fix tests

---
+++ src/cards/Card_Artisan.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import unittest
+from Card import Card
+
+
+###############################################################################
+class Card_Artisan(Card):
+    def __init__(self):
+        Card.__init__(self)
+        self.cardtype = 'action'
+        self.base = 'dominion'
+        self.desc = "Gain a card to your hand costing up to 5 Coin. Put a card from your hand onto your deck."
+        self.name = 'Artisan'
+        self.cost = 6
+
+    def special(self, game, player):
+        player.plrGainCard(5, destination='hand')
+        card = player.cardSel(force=True, cardsrc='hand', prompt="Put a card from your hand on top of your deck")
+        player.addCard(card[0], 'topdeck')
+        player.hand.remove(card[0])
+
+
+###############################################################################
+class Test_Artisan(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Artisan', 'Festival'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Artisan'].remove()
+
+    def test_play(self):
+        self.plr.setHand('Copper', 'Estate', 'Silver', 'Gold', 'Province')
+        self.plr.addCard(self.card, 'hand')
+        self.plr.test_input = ['Get Festival', 'Select Gold']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.handSize(), 5)
+        self.assertEqual(self.plr.discardSize(), 0)
+        self.assertEqual(self.plr.deck[-1].name, 'Gold')
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
