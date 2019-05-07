a3243bedde9a544d7ee71ead48856e7e33cc8894
Dougal Scott <dougal.scott@gmail.com>
2019-05-07 10:44:33 +1000

    Implemented Citadel

---
+++ src/projects/Project_Citadel.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import unittest
+from Project import Project
+
+
+###############################################################################
+class Project_Citadel(Project):
+    def __init__(self):
+        Project.__init__(self)
+        self.cardtype = 'project'
+        self.base = 'renaissance'
+        self.desc = "The first time you play an Action card during each of your turns, play it again afterward."
+        self.name = "Citadel"
+        self.cost = 8
+
+    def hook_postAction(self, game, player, card):
+        if player.playedSize() == 1:
+            player.output("Citadel plays {} again".format(card.name))
+            player.playCard(card, discard=False, costAction=False, postActionHook=False)
+
+
+###############################################################################
+class Test_Citadel(unittest.TestCase):
+    def setUp(self):
+        import Game
+        self.g = Game.Game(quiet=True, numplayers=1, initprojects=['Citadel'], initcards=['Moat'])
+        self.g.startGame()
+        self.plr = self.g.playerList(0)
+        self.card = self.g['Moat'].remove()
+        self.plr.addCard(self.card, 'hand')
+
+    def test_play(self):
+        self.plr.assign_project('Citadel')
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.handSize(), 5 + 2 + 2)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
