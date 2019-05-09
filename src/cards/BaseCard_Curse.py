928c9da19ca5cb5a735db2fb02ce185ba25f3968
Dougal Scott <dougal.scott@gmail.com>
2019-05-10 08:54:26 +1000

    Curses need to be purchaseable for defiled shrine

--- src/cards/BaseCard_Curse.py
+++ src/cards/BaseCard_Curse.py
@@ -13,7 +13,7 @@ class Card_Curse(Card):
         self.desc = "-1 VP"
         self.basecard = True
         self.playable = False
-        self.purchasable = False
+        self.purchasable = True
         self.name = 'Curse'
         self.cost = 0
         self.victory = -1
