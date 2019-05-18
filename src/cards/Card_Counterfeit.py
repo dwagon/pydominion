371e7676b9089a9ade58401a72cee49b94aa26e4
Dougal Scott <dougal.scott@gmail.com>
2019-05-18 18:21:14 +1000

    Fix desc

--- src/cards/Card_Counterfeit.py
+++ src/cards/Card_Counterfeit.py
@@ -9,7 +9,7 @@ class Card_Counterfeit(Card):
         Card.__init__(self)
         self.cardtype = 'treasure'
         self.base = 'darkages'
-        self.desc = "+1 Gold, +1 Buy; May play a treasure twice and trash it"
+        self.desc = "+1 Coin, +1 Buy; May play a treasure twice and trash it"
         self.name = 'Counterfeit'
         self.cost = 5
         self.coin = 1
