8db58cbd621b0d8643df2044738a7a02048bea05
Dougal Scott <dougal.scott@gmail.com>
2020-06-30 19:17:10 +1000

    pylint required

--- src/cards/Card_Apothecary.py
+++ src/cards/Card_Apothecary.py
@@ -4,6 +4,7 @@ import unittest
 from Card import Card
 
 
+###############################################################################
 class Card_Apothecary(Card):
     def __init__(self):
         Card.__init__(self)
