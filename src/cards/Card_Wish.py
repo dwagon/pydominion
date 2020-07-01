47c65656efd58818c8f8bf391dc395148982d4a2
Dougal Scott <dougal.scott@gmail.com>
2020-07-01 16:58:17 +1000

    Implement Horse

--- src/cards/Card_Wish.py
+++ src/cards/Card_Wish.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
+import Game
 from Card import Card
 
 
@@ -34,7 +35,6 @@ class Card_Wish(Card):
 ###############################################################################
 class Test_Wish(unittest.TestCase):
     def setUp(self):
-        import Game
         self.g = Game.Game(quiet=True, numplayers=1, initcards=["Wish"])
         self.g.start_game()
         self.plr = self.g.player_list(0)
