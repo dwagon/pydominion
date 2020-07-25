f924b43453725b51db58a649ab05e2a281d8c9dd
Dougal Scott <dougal.scott@gmail.com>
2020-07-25 10:39:22 +1000

    Lint

--- src/cards/Card_Tactician.py
+++ src/cards/Card_Tactician.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
+import Game
 from Card import Card
 
 
@@ -37,7 +38,6 @@ class Card_Tactician(Card):
 ###############################################################################
 class Test_Tactician(unittest.TestCase):
     def setUp(self):
-        import Game
         self.g = Game.Game(quiet=True, numplayers=1, initcards=['Tactician'])
         self.g.start_game()
         self.plr = self.g.player_list(0)
