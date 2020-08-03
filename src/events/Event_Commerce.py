1cc57b193fcc796b21be6c3b0405688eba418065
Dougal Scott <dougal.scott@gmail.com>
2020-08-03 15:44:20 +1000

    Implemented Commerce

---
+++ src/events/Event_Commerce.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import unittest
+import Game
+from Event import Event
+
+
+###############################################################################
+class Event_Commerce(Event):
+    def __init__(self):
+        Event.__init__(self)
+        self.base = 'menagerie'
+        self.desc = "Gain a Gold per differently named card you've gained this turn."
+        self.name = "Commerce"
+        self.cost = 5
+
+    def special(self, game, player):
+        gains = {_.name for _ in player.stats['gained']}
+        for _ in gains:
+            player.gainCard('Gold')
+
+
+###############################################################################
+class Test_Commerce(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(quiet=True, numplayers=1, eventcards=['Commerce'], initcards=['Moat'])
+        self.g.start_game()
+        self.plr = self.g.player_list()[0]
+        self.card = self.g.events['Commerce']
+
+    def test_Commerce(self):
+        """ Use Commerce """
+        self.plr.addCoin(5)
+        self.plr.gainCard('Moat')
+        self.plr.performEvent(self.card)
+        self.g.print_state()
+        self.assertIsNotNone(self.plr.inDiscard('Gold'))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
