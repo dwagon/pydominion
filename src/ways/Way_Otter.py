11843962dd8e0ff8d41462f3a06cf0b13bb760ab
Dougal Scott <dougal.scott@gmail.com>
2020-08-03 11:00:21 +1000

    Implemented Otter

---
+++ src/ways/Way_Otter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import unittest
+import Game
+from Way import Way
+
+
+###############################################################################
+class Way_Otter(Way):
+    def __init__(self):
+        Way.__init__(self)
+        self.base = 'menagerie'
+        self.desc = "+2 Cards"
+        self.name = "Otter"
+        self.cards = 2
+
+
+###############################################################################
+class Test_Otter(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(quiet=True, numplayers=1, waycards=['Otter'], initcards=['Moat'], badcards=["Duchess"])
+        self.g.start_game()
+        self.plr = self.g.player_list(0)
+        self.card = self.g['Moat'].remove()
+
+    def test_play(self):
+        """ Perform a Otter """
+        self.plr.addCard(self.card, 'hand')
+        self.plr.test_input = ['Otter']
+        self.plr.playCard(self.card)
+        self.assertEqual(self.plr.handSize(), 5 + 2)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
