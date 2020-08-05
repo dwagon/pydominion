94b657c5f8ca5c6c42905ec8fb44b237d3837ab1
Dougal Scott <dougal.scott@gmail.com>
2020-08-05 11:32:14 +1000

    Tell player reason for failure

---
+++ src/events/Event_Stampede.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import unittest
+import Game
+from Event import Event
+
+
+###############################################################################
+class Event_Stampede(Event):
+    def __init__(self):
+        Event.__init__(self)
+        self.base = 'menagerie'
+        self.desc = "If you have 5 or fewer cards in play, gain 5 Horses onto your deck."
+        self.name = "Stampede"
+        self.cost = 5
+        self.required_cards = [('Card', 'Horse')]
+
+    def special(self, game, player):
+        if player.played_size() <= 5:
+            for _ in range(5):
+                player.gainCard('Horse')
+        else:
+            player.output("You have played too many cards this turn")
+
+
+###############################################################################
+class Test_Stampede(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(
+            quiet=True, numplayers=1, eventcards=['Stampede'],
+            initcards=['Moat'], badcards=['Hostelry', 'Border Village', 'Inn']
+        )
+        self.g.start_game()
+        self.plr = self.g.player_list()[0]
+        self.card = self.g.events['Stampede']
+
+    def test_Stampede(self):
+        """ Use Stampede """
+        self.plr.addCoin(5)
+        self.plr.performEvent(self.card)
+        self.assertIsNotNone(self.plr.in_discard('Horse'))
+        self.assertEqual(self.plr.discard_size(), 5)
+
+    def test_no_Stampede(self):
+        """ Use Stampede with played lots """
+        self.plr.setPlayed('Copper', 'Silver', 'Gold', 'Copper', 'Silver', 'Gold')
+        self.plr.addCoin(5)
+        self.plr.performEvent(self.card)
+        self.assertIsNone(self.plr.in_discard('Horse'))
+        self.assertEqual(self.plr.discard_size(), 0)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
