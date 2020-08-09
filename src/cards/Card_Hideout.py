a3fff4d3016f9d3e9044fb4c577c19c46e6bea72
Dougal Scott <dougal.scott@gmail.com>
2020-08-09 13:49:28 +1000

    Trashing is not optional

--- src/cards/Card_Hideout.py
+++ src/cards/Card_Hideout.py
@@ -20,7 +20,7 @@ class Card_Hideout(Card.Card):
 
     ###########################################################################
     def special(self, game, player):
-        card = player.plrTrashCard(num=1)
+        card = player.plrTrashCard(num=1, force=True)
         if card[0].isVictory():
             player.gainCard('Curse')
 
