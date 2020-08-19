52c9c754e3e27aab9a9ee230b2c9275c951d3520
Dougal Scott <dougal.scott@gmail.com>
2020-08-20 08:55:52 +1000

    Start to complete

--- src/cards/RuinCard_Survivors.py
+++ src/cards/RuinCard_Survivors.py
@@ -12,12 +12,23 @@ class Card_Survivors(Card.Card):
         self.base = Game.DARKAGES
         self.purchasable = False
         self.cost = 0
-        self.desc = "Look at top 2 cards of deck. Discard or retain on deck"
+        self.desc = "Look at the top 2 cards of your deck. Discard them or put them back in any order."
         self.name = "Survivors"
 
     def special(self, game, player):
         """ Look at the top 2 cards of your deck. Discard them or
             put them back in any order """
-        # TODO
+        crds = player.pickupCards(2)
+        ans = player.plrChooseOptions(
+            "What to do with survivors?",
+            ("Discard {} and {}".format(crds[0].name, crds[1].name), 'discard'),
+            ("Return {} and {} to deck".format(crds[0].name, crds[1].name), 'return')
+        )
+        if ans == 'discard':
+            player.discardCard(crds[0])
+            player.discardCard(crds[1])
+        else:
+            player.addCard(crds[0], 'deck')
+            player.addCard(crds[1], 'deck')
 
 # EOF
