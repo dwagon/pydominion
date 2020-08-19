30f7ddc146dc4df75a04b71b79fb92d8f449b0c3
Dougal Scott <dougal.scott@gmail.com>
2020-08-20 09:31:07 +1000

    More ruin cards to enure we have the one we want

--- src/cards/RuinCard_Survivors.py
+++ src/cards/RuinCard_Survivors.py
@@ -37,7 +37,7 @@ class Card_Survivors(Card.Card):
 ###############################################################################
 class Test_Survivors(unittest.TestCase):
     def setUp(self):
-        self.g = Game.Game(quiet=True, numplayers=1, initcards=['Cultist'])
+        self.g = Game.Game(quiet=True, numplayers=4, initcards=['Cultist'])
         self.g.start_game()
         self.plr = self.g.player_list(0)
         while True:
