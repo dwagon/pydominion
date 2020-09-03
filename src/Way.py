80c977c4f4d3cb7136c34226762ad9cfb3a1c67a
Dougal Scott <dougal.scott@gmail.com>
2020-09-03 17:20:33 +1000

    Implemented hook_way_discard_this_card

--- src/Way.py
+++ src/Way.py
@@ -1,8 +1,14 @@
+""" http://wiki.dominionstrategy.com/index.php/Way """
 from Card import Card
 
 
 class Way(Card):
     def special_way(self, game, player, card):
+        """ Special hook for ways that include the triggering card """
+        pass
+
+    def hook_way_discard_this_card(self, game, player, card):
+        """ Hook called when the card that the way was triggered through is discarded """
         pass
 
 # EOF
