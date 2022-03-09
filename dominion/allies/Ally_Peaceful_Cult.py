c1ce74c6ad3335dc6137559d7dca5f28aa21dca1
Dougal Scott <dougal.scott@gmail.com>
2022-03-09 19:29:43 +1100

    Implemented Peaceful Cult

---
+++ dominion/allies/Ally_Peaceful_Cult.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+""" http://wiki.dominionstrategy.com/index.php/Peaceful_Cult """
+
+import unittest
+from dominion import Game, Ally
+
+
+###############################################################################
+class Ally_Peaceful_Cult(Ally.Ally):
+    def __init__(self):
+        Ally.Ally.__init__(self)
+        self.base = Game.ALLIES
+        self.desc = """At the start of your Buy phase, you may spend
+            any number of Favors to trash that many cards from your hand."""
+        self.name = "Peaceful Cult"
+
+    def hook_preBuy(self, game, player):
+        if not player.getFavor():
+            return
+        player.output("Use Peaceful Cult to trash a card per favor")
+        trshed = player.plrTrashCard(num=player.getFavor())
+        player.addFavor(-1 * len(trshed))
+
+
+###############################################################################
+class Test_Peaceful_Cult(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(quiet=True, numplayers=1, ally="Peaceful Cult", initcards=["Underling"])
+        self.g.start_game()
+        self.plr = self.g.player_list(0)
+
+    def test_flag(self):
+        self.plr.setHand("Copper", "Silver", "Gold")
+        self.plr.setFavor(2)
+        self.plr.test_input = ["Trash Copper", "Finish", "End Phase"]
+        self.plr.buy_phase()
+        self.assertEqual(self.plr.getFavor(), 1)
+        self.assertTrue(self.g.in_trash("Copper"))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
