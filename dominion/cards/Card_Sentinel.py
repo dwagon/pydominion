67fde2d123f66e644e4da8c5925ed0a4d3d6680a
Dougal Scott <dougal.scott@gmail.com>
2022-03-15 09:43:45 +1100

    Implemented Sentinel

---
+++ dominion/cards/Card_Sentinel.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""http://wiki.dominionstrategy.com/index.php/Sentinel"""
+
+import unittest
+from dominion import Game, Card
+
+
+###############################################################################
+class Card_Sentinel(Card.Card):
+    def __init__(self):
+        Card.Card.__init__(self)
+        self.cardtype = Card.TYPE_ACTION
+        self.base = Game.ALLIES
+        self.name = "Sentinel"
+        self.desc = "Look at the top 5 cards of your deck. You may trash up to 2 of them. Put the rest back in any order."
+        self.cost = 3
+
+    def special(self, game, player):
+        cards = []
+        for _ in range(5):
+            cards.append(player.next_card())
+        player.output("Trash up to 2 of these")
+        trashed = player.plr_trash_card(num=2, cardsrc=cards)
+        for card in cards:
+            if card not in trashed:
+                player.add_card(card, "topdeck")
+
+
+###############################################################################
+class Test_Sentinel(unittest.TestCase):
+    def setUp(self):
+        self.g = Game.Game(quiet=True, numplayers=1, initcards=["Sentinel"])
+        self.g.start_game()
+        self.plr = self.g.player_list()[0]
+        self.card = self.g["Sentinel"].remove()
+        self.plr.add_card(self.card, "hand")
+
+    def test_play(self):
+        """Play the card"""
+        self.plr.set_deck("Province", "Copper", "Silver", "Gold", "Estate", "Duchy")
+        self.plr.test_input = ["Trash Copper", "Finish"]
+        self.plr.play_card(self.card)
+        self.assertIsNotNone(self.g.in_trash("Copper"))
+        self.assertIsNotNone(self.plr.in_deck("Silver"))
+        self.assertIsNotNone(self.plr.in_deck("Gold"))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
