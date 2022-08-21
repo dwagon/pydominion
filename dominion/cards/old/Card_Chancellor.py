093807be0bda311282c1e44ac3018fc6353a8dfa
Dougal Scott <dougal.scott@gmail.com>
2022-08-21 18:22:03 +1000

    Retire chancellor

--- dominion/cards/old/Card_Chancellor.py
+++ dominion/cards/old/Card_Chancellor.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+""" http://wiki.dominionstrategy.com/index.php/Chancellor """
 
 import unittest
-import Game
-import Card
+from dominion import Card, Game
 
 
 ###############################################################################
 class Card_Chancellor(Card.Card):
+    """ Chancellor """
     def __init__(self):
         Card.Card.__init__(self)
         self.cardtype = Card.TYPE_ACTION
@@ -16,29 +17,32 @@ class Card_Chancellor(Card.Card):
         self.coin = 2
         self.cost = 3
 
-    def special(self, game, player):
-        ans = player.plr_choose_options(
+    def special(self, game, player):    # pylint: disable=unused-argument
+        """ Chancellor Special """
+        disc = player.plr_choose_options(
             "Discard deck?", ("Don't Discard", False), ("Discard Deck", True)
         )
-        if ans:
-            for c in player.deck[:]:
-                player.add_card(c, "discard")
-                player.deck.remove(c)
+        if disc:
+            for crd in player.deck:
+                player.add_card(crd, "discard")
+                player.deck.remove(crd)
 
 
 ###############################################################################
 class Test_Chancellor(unittest.TestCase):
+    """ Test Chancellor """
     def setUp(self):
-        self.g = Game.TestGame(numplayers=1, initcards=["Chancellor"])
+        self.g = Game.TestGame(numplayers=1, oldcards=True, initcards=["Chancellor"])
         self.g.start_game()
         self.plr = self.g.player_list(0)
         self.ccard = self.g["Chancellor"].remove()
-        self.plr.set_hand("Estate")
+        self.plr.hand.set("Estate")
         self.plr.add_card(self.ccard, "hand")
 
     def test_nodiscard(self):
-        self.plr.set_deck("Copper", "Silver", "Gold")
-        self.plr.set_discard("Estate", "Duchy", "Province")
+        """ Play Chancellor and choose not to discard """
+        self.plr.deck.set("Copper", "Silver", "Gold")
+        self.plr.discardpile.set("Estate", "Duchy", "Province")
         self.plr.test_input = ["Don't discard"]
         self.plr.play_card(self.ccard)
         self.assertEqual(self.plr.get_coins(), 2)
@@ -46,8 +50,9 @@ class Test_Chancellor(unittest.TestCase):
         self.assertEqual(self.plr.discardpile.size(), 3)
 
     def test_discard(self):
-        self.plr.set_deck("Copper", "Silver", "Gold")
-        self.plr.set_discard("Estate", "Duchy", "Province")
+        """ Play Chancellor and choose to discard deck """
+        self.plr.deck.set("Copper", "Silver", "Gold")
+        self.plr.discardpile.set("Estate", "Duchy", "Province")
         self.plr.test_input = ["discard deck"]
         self.plr.play_card(self.ccard)
         self.assertEqual(self.plr.get_coins(), 2)
