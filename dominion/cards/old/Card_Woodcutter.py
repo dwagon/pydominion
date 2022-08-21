7ce58cb6b2f3a715e2a68cc042510911b696a60c
Dougal Scott <dougal.scott@gmail.com>
2022-08-21 18:31:38 +1000

    Retire Wood Cutter

--- dominion/cards/old/Card_Woodcutter.py
+++ dominion/cards/old/Card_Woodcutter.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+""" http://wiki.dominionstrategy.com/index.php/Woodcutter"""
 
 import unittest
-import Game
-import Card
+from dominion import Card, Game
 
 
 ###############################################################################
 class Card_Woodcutter(Card.Card):
+    """ Woodcutter"""
     def __init__(self):
         Card.Card.__init__(self)
         self.cardtype = Card.TYPE_ACTION
@@ -20,8 +21,9 @@ class Card_Woodcutter(Card.Card):
 
 ###############################################################################
 class Test_Woodcutter(unittest.TestCase):
+    """ Test Woodcutter """
     def setUp(self):
-        self.g = Game.TestGame(numplayers=1, initcards=["Woodcutter"])
+        self.g = Game.TestGame(numplayers=1, oldcards=True, initcards=["Woodcutter"])
         self.g.start_game()
         self.plr = self.g.player_list(0)
         self.card = self.g["Woodcutter"].remove()
@@ -31,7 +33,7 @@ class Test_Woodcutter(unittest.TestCase):
         """Play the woodcutter"""
         self.plr.play_card(self.card)
         self.assertEqual(self.plr.get_coins(), 2)
-        self.assertEqual(self.plr.getBuys(), 2)
+        self.assertEqual(self.plr.get_buys(), 2)
 
 
 ###############################################################################
