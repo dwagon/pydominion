5c3835b35d3eb95216b65787a85f8468ce126146
Dougal Scott <dougal.scott@gmail.com>
2022-08-28 22:12:58 +1000

    Retired a lot of cards for second edition of hinterlands

--- dominion/cards/old/Card_Mandarin.py
+++ dominion/cards/old/Card_Mandarin.py
@@ -40,7 +40,7 @@ class Card_Mandarin(Card.Card):
 ###############################################################################
 class Test_Mandarin(unittest.TestCase):
     def setUp(self):
-        self.g = Game.TestGame(numplayers=1, initcards=["Mandarin"])
+        self.g = Game.TestGame(numplayers=1, oldcards=True, initcards=["Mandarin"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.card = self.g["Mandarin"].remove()
