83db1b2904d60d58ae69559692d06d7866e68132
Dougal Scott <dougal.scott@gmail.com>
2022-09-06 11:17:34 +1000

    Implemented Shelters

---
+++ dominion/cards/Shelter_Hovel.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+""" http://wiki.dominionstrategy.com/index.php/Hovel """
+
+import unittest
+from dominion import Card, Game
+
+
+###############################################################################
+class Card_Hovel(Card.Card):
+    """ Hovel """
+    def __init__(self):
+        Card.Card.__init__(self)
+        self.cardtype = [Card.TYPE_REACTION, Card.TYPE_SHELTER]
+        self.base = Game.DARKAGES
+        self.desc = "When you gain a Victory card, you may trash this from your hand."
+        self.name = "Hovel"
+        self.cost = 1
+        self.purchasable = False
+        self.victory = 0
+
+    def hook_gain_card(self, game, player, card):
+        if not card.isVictory():
+            return
+        to_trash = player.plr_choose_options("Trash Hovel?", ("Trash it", True), ("Keep it", False))
+        if to_trash:
+            player.trash_card(self)
+
+
+###############################################################################
+def botresponse(player, kind, args=None, kwargs=None):  # pragma: no cover, pylint: disable=unused-argument
+    """ botresponse """
+    return True
+
+
+###############################################################################
+class Test_Hovel(unittest.TestCase):
+    """ Test Hovel """
+    def setUp(self):
+        self.g = Game.TestGame(quiet=True, numplayers=1, initcards=["Shelters"])
+        self.g.start_game()
+        self.plr = self.g.player_list(0)
+        self.card = self.g["Hovel"].remove()
+
+    def test_trash(self):
+        """ Test Trashing """
+        self.plr.hand.set("Hovel")
+        self.plr.test_input = ["Trash it"]
+        self.plr.gain_card("Province")
+        self.assertIsNotNone(self.g.in_trash("Hovel"))
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
--- dominion/cards/Shelter_Necropolis.py
+++ dominion/cards/Shelter_Necropolis.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python
-""" http://wiki.dominionstrategy.com/index.php/Overgrown_Estate """
+""" http://wiki.dominionstrategy.com/index.php/Necropolis """
 
 import unittest
 from dominion import Card, Game
 
 
 ###############################################################################
-class Card_Overgrown_Estate(Card.Card):
-    """ Overgrown Estate """
+class Card_Necropolis(Card.Card):
+    """ Necropolis """
     def __init__(self):
         Card.Card.__init__(self)
-        self.cardtype = [Card.TYPE_VICTORY, Card.TYPE_SHELTER]
+        self.cardtype = [Card.TYPE_ACTION, Card.TYPE_SHELTER]
         self.base = Game.DARKAGES
-        self.desc = "0VP; When you trash this, +1 Card."
-        self.name = "Overgrown Estate"
+        self.desc = "0VP; +2 Actions"
+        self.name = "Necropolis"
         self.cost = 1
+        self.actions = 2
         self.victory = 0
         self.purchasable = False
 
-    def hook_trashThisCard(self, game, player):
-        player.pickup_card()
-
 
 ###############################################################################
-class Test_Overgrown_Estate(unittest.TestCase):
-    """ Test Overgrown Estate """
+class Test_Necropolis(unittest.TestCase):
+    """ Test Necropolis """
     def setUp(self):
         self.g = Game.TestGame(quiet=True, numplayers=1, initcards=["Shelters"])
         self.g.start_game()
@@ -32,12 +30,11 @@ class Test_Overgrown_Estate(unittest.TestCase):
 
     def test_play(self):
         """ Test Play """
-        self.plr.deck.set("Province")
-        self.plr.hand.set("Overgrown Estate")
-        card = self.plr.hand["Overgrown Estate"]
-        self.plr.trash_card(card)
-        self.assertIn("Province", self.plr.hand)
-        self.assertIn("Overgrown Estate", self.g.trashpile)
+        self.plr.hand.set("Necropolis", "Estate")
+        card = self.plr.hand["Necropolis"]
+        self.assertEqual(self.plr.get_actions(), 1)
+        self.plr.play_card(card)
+        self.assertEqual(self.plr.get_actions(), 2)
 
 
 ###############################################################################
