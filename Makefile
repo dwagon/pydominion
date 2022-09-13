b54d76b49b8ea77569ee1564efe1571eb68352ee
Dougal Scott <dougal.scott@gmail.com>
2022-09-14 09:33:03 +1000

    Implemented Monkey

--- Makefile
+++ Makefile
@@ -11,7 +11,6 @@ pex:
 venv:
 	python3 -m venv ${VENV_HOME}
 	${VENV_HOME}/bin/pip install -r requirements-dev.txt
-	${VENV_HOME}/bin/pip install -r requirements.txt
 
 clean:
 	/bin/rm -rf ${VENV_HOME}
---
+++ dominion/cards/Card_Monkey.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+""" http://wiki.dominionstrategy.com/index.php/Monkey """
+
+import unittest
+from dominion import Card, Game
+
+
+###############################################################################
+class Card_Monkey(Card.Card):
+    """Monkey"""
+
+    def __init__(self):
+        Card.Card.__init__(self)
+        self.cardtype = [Card.TYPE_ACTION, Card.TYPE_DURATION]
+        self.base = Game.SEASIDE
+        self.desc = """Until your next turn, when the player to your right gains a card,
+            +1 Card.  At the start of your next turn, +1 Card."""
+        self.name = "Monkey"
+        self.cost = 3
+
+    def hook_allplayers_gain_card(self, game, player, owner, card):
+        """Until your next turn, when the player to your right gains a card, +1 Card"""
+        if player == game.playerToRight(owner):
+            owner.pickup_card()
+
+    def duration(self, game, player):
+        """At the start of your next turn, +1 Card."""
+        player.pickup_card()
+
+
+###############################################################################
+class Test_Monkey(unittest.TestCase):
+    """Test Monkey"""
+
+    def setUp(self):
+        self.g = Game.TestGame(numplayers=2, initcards=["Monkey", "Moat"])
+        self.g.start_game()
+        self.plr, self.oth = self.g.player_list()
+        self.card = self.g["Monkey"].remove()
+        self.plr.add_card(self.card, "hand")
+
+    def test_play(self):
+        """Play Monkey"""
+        self.plr.play_card(self.card)
+        self.plr.end_turn()
+        self.oth.gain_card("Moat")
+        self.assertEqual(self.plr.hand.size(), 5 + 1)
+        self.plr.start_turn()
+        self.assertEqual(self.plr.hand.size(), 5 + 1 + 1)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
