c43239b12d7e4a7000b1a346bce65e1edb66bfee
Dougal Scott <dougal.scott@gmail.com>
2023-08-10 09:29:00 +1000

    Handle case where the suitable treasure cardpile is empty

--- dominion/cards/Card_Mine.py
+++ dominion/cards/Card_Mine.py
@@ -19,10 +19,10 @@ class Card_Mine(Card.Card):
         costing up to 3 more, put it in your hand"""
         options = [{"selector": "0", "print": "Don't trash a card", "card": None}]
         index = 1
-        for c in player.hand:
-            if c.isTreasure():
+        for card in player.hand:
+            if card.isTreasure():
                 sel = "%s" % index
-                options.append({"selector": sel, "print": f"Trash/Upgrade {c.name}", "card": c})
+                options.append({"selector": sel, "print": f"Trash/Upgrade {card.name}", "card": card})
                 index += 1
         player.output("Trash a treasure to gain a better one")
         o = player.user_input(options, "Trash which treasure?")
@@ -32,8 +32,11 @@ class Card_Mine(Card.Card):
             # TODO - let user pick
             for tc in game._base_cards:
                 if game[tc].cost == val + 3:
-                    c = player.gain_card(tc, "hand")
-                    player.output(f"Converted to {c.name}")
+                    card = player.gain_card(tc, "hand")
+                    if not card:
+                        player.output("No suitable cards left")
+                        break
+                    player.output(f"Converted to {card.name}")
                     player.trash_card(o["card"])
                     break
             else:  # pragma: no cover
@@ -41,14 +44,14 @@ class Card_Mine(Card.Card):
 
 
 ###############################################################################
-class Test_Mine(unittest.TestCase):
+class TestMine(unittest.TestCase):
     def setUp(self):
         self.g = Game.TestGame(numplayers=1, initcards=["Mine"])
         self.g.start_game()
         self.plr = self.g.player_list(0)
         self.card = self.g["Mine"].remove()
 
-    def test_convcopper(self):
+    def test_convert_copper(self):
         self.plr.hand.set("Copper")
         self.plr.add_card(self.card, "hand")
         self.plr.test_input = ["1"]
@@ -60,13 +63,25 @@ class Test_Mine(unittest.TestCase):
         self.assertEqual(self.plr.buys.get(), 1)
         self.assertEqual(self.plr.actions.get(), 0)
 
-    def test_convnothing(self):
+    def test_convert_nothing(self):
         self.plr.hand.set("Copper")
         self.plr.add_card(self.card, "hand")
         self.plr.test_input = ["0"]
         self.plr.play_card(self.card)
         self.assertEqual(self.plr.hand[0].name, "Copper")
         self.assertTrue(self.plr.discardpile.is_empty())
+        self.assertEqual(self.plr.hand.size(), 1)
+
+    def test_convert_nothing_left(self):
+        """Test where the destination treasure is no longer available"""
+        while self.g["Silver"]:
+            self.plr.gain_card("Silver")
+        self.plr.hand.set("Copper")
+        self.plr.add_card(self.card, "hand")
+        self.plr.test_input = ["1"]
+        self.plr.play_card(self.card)
+        self.assertEqual(self.plr.hand[0].name, "Copper")
+        self.assertFalse(self.plr.discardpile.is_empty())
         self.assertEqual(self.plr.hand.size(), 1)
 
 
