e6594f688481a3130354a98b8f257f2d62c2a714
Dougal Scott <dougal.scott@gmail.com>
2023-09-27 09:17:40 +1000

    Implemented Setters / Bustling Village split

--- dominion/cards/Card_Settlers.py
+++ dominion/cards/Card_Settlers.py
@@ -1,61 +1,56 @@
 #!/usr/bin/env python
-
+""" https://wiki.dominionstrategy.com/index.php/Settlers
+    https://wiki.dominionstrategy.com/index.php/Bustling_Village"""
 import unittest
-from dominion import Game, Card, Piles
-import dominion.Card as Card
+from dominion import Card, Game, CardPile
 
 
 ###############################################################################
-class Card_Settlers(Card.Card):
+class Card_PatricianSplit(Card.Card):
     def __init__(self):
         Card.Card.__init__(self)
-        self.cardtype = Card.CardType.ACTION
-        self.base = Card.CardExpansion.EMPIRES
         self.name = "Settlers"
-        self.cards = 1
-        self.actions = 1
-        self.cost = 2
-        self.desc = """+1 Card +1 Action. Look through your discard pile. You may reveal a Copper from it and put it into your hand."""
+        self.base = Card.CardExpansion.EMPIRES
 
-    def special(self, game, player):
-        cu = player.piles[Piles.DISCARD]["Copper"]
-        if cu:
-            player.add_card(cu, Piles.HAND)
-            player.piles[Piles.DISCARD].remove(cu)
-            player.reveal_card(cu)
-            player.output("Pulled Copper from discard into hand")
-        else:
-            player.output("No Copper in discard")
+    @classmethod
+    def cardpile_setup(cls, game):
+        card_pile = SettlersCardPile(game)
+        return card_pile
 
 
 ###############################################################################
-class Test_Settlers(unittest.TestCase):
+class SettlersCardPile(CardPile.CardPile):
+    def __init__(self, game):
+        mapping = game.get_card_classes("Split", game.paths["cards"], "Card_")
+        for name, class_ in mapping.items():
+            game.card_instances[name] = class_()
+        super().__init__()
+
+    def init_cards(self, num_cards=0, card_class=None):
+        # pylint: disable=import-outside-toplevel
+        from dominion.cards.Split_Settlers import Card_Settlers
+        from dominion.cards.Split_Bustling_Village import Card_BustlingVillage
+
+        for card_class in (Card_Settlers, Card_BustlingVillage):
+            for _ in range(5):
+                self.cards.insert(0, card_class())
+
+
+###############################################################################
+class TestEncampmentPile(unittest.TestCase):
     def setUp(self):
         self.g = Game.TestGame(numplayers=1, initcards=["Settlers"])
         self.g.start_game()
         self.plr = self.g.player_list(0)
-        self.card = self.g.get_card_from_pile("Settlers")
 
-    def test_play(self):
-        """Play a Settlers and pull a copper"""
-        self.plr.piles[Piles.DISCARD].set("Gold", "Silver", "Copper")
-        self.plr.piles[Piles.HAND].set("Gold", "Silver")
-        self.plr.add_card(self.card, Piles.HAND)
-        self.plr.play_card(self.card)
-        self.assertIn("Copper", self.plr.piles[Piles.HAND])
-        self.assertNotIn("Copper", self.plr.piles[Piles.DISCARD])
-        self.assertEqual(self.plr.actions.get(), 1)
-        self.assertEqual(self.plr.piles[Piles.HAND].size(), 2 + 1 + 1)
-
-    def test_play_nocopper(self):
-        """Play a Settlers and pull a copper"""
-        self.plr.piles[Piles.DECK].set("Gold", "Silver")
-        self.plr.piles[Piles.DISCARD].set("Gold", "Silver", "Duchy")
-        self.plr.piles[Piles.HAND].set("Gold", "Silver")
-        self.plr.add_card(self.card, Piles.HAND)
-        self.plr.play_card(self.card)
-        self.assertNotIn("Copper", self.plr.piles[Piles.HAND])
-        self.assertEqual(self.plr.piles[Piles.HAND].size(), 2 + 1)
+    def test_pile(self):
+        self.assertEqual(len(self.g.card_piles["Settlers"]), 10)
+        card = self.g.get_card_from_pile("Settlers")
+        self.assertEqual(card.name, "Settlers")
+        self.assertEqual(len(self.g.card_piles["Settlers"]), 9)
+        for _ in range(5):
+            card = self.g.get_card_from_pile("Settlers")
+        self.assertEqual(card.name, "Bustling Village")
 
 
 ###############################################################################
