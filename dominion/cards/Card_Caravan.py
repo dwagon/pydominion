b34629a3f6072538f172c60a1d0a9e69fec01d65
Dougal Scott <dougal.scott@gmail.com>
2023-10-10 12:44:12 +1100

    Another shaman bad interaction

--- dominion/cards/Card_Caravan.py
+++ dominion/cards/Card_Caravan.py
@@ -25,7 +25,7 @@ class Card_Caravan(Card.Card):
 ###############################################################################
 class Test_Caravan(unittest.TestCase):
     def setUp(self):
-        self.g = Game.TestGame(numplayers=1, initcards=["Caravan"])
+        self.g = Game.TestGame(numplayers=1, initcards=["Caravan"], badcards=["Shaman"])
         self.g.start_game()
         self.plr = self.g.player_list(0)
         self.card = self.g.get_card_from_pile("Caravan")
