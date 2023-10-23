541b1b8e25b25467d886865c17eec6b234af59ec
Dougal Scott <dougal.scott@gmail.com>
2023-10-23 17:24:56 +1100

    Fix when no cards are available to trash

--- dominion/cards/Card_Crucible.py
+++ dominion/cards/Card_Crucible.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """ https://wiki.dominionstrategy.com/index.php/Crucible"""
 import unittest
-from dominion import Card, Game, Piles
+from dominion import Card, Game, Piles, Player
 
 
 ###############################################################################
 class Card_Crucible(Card.Card):
     """Crucible"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         Card.Card.__init__(self)
         self.cardtype = Card.CardType.TREASURE
         self.base = Card.CardExpansion.PLUNDER
@@ -16,23 +16,23 @@ class Card_Crucible(Card.Card):
         self.name = "Crucible"
         self.cost = 4
 
-    def special(self, game, player):
-        card = player.plr_trash_card(force=True)
-        player.output(f"Gained {card[0].cost} coin")
-        player.coins.add(card[0].cost)
+    def special(self, game: Game.Game, player: Player.Player) -> None:
+        if cards := player.plr_trash_card(force=True):
+            player.output(f"Gained {cards[0].cost} coin")
+            player.coins.add(cards[0].cost)
 
 
 ###############################################################################
-class Test_Crucible(unittest.TestCase):
+class TestCrucible(unittest.TestCase):
     """Test Crucible"""
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.g = Game.TestGame(numplayers=1, initcards=["Crucible"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.card = self.g.get_card_from_pile("Crucible")
 
-    def test_play(self):
+    def test_play(self) -> None:
         """Play a salvage"""
         self.plr.piles[Piles.HAND].set("Duchy", "Estate")
         self.plr.add_card(self.card, Piles.HAND)
