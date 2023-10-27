7738989a426f81cbc5096473aa8a718c5c847a05
Dougal Scott <dougal.scott@gmail.com>
2023-10-28 09:03:11 +1100

    Fix Silver Mine causing test failure

--- dominion/cards/Card_Camel_Train.py
+++ dominion/cards/Card_Camel_Train.py
@@ -2,24 +2,24 @@
 """ http://wiki.dominionstrategy.com/index.php/Camel_Train """
 
 import unittest
-from dominion import Card, Game, Piles, Player
+from dominion import Card, Game, Piles, Player, Phase
 
 
 ###############################################################################
 class Card_Camel_Train(Card.Card):
-    def __init__(self):
+    def __init__(self) -> None:
         Card.Card.__init__(self)
         self.cardtype = Card.CardType.ACTION
         self.base = Card.CardExpansion.MENAGERIE
         self.name = "Camel Train"
         self.cost = 3
 
-    def desc(self, player):
-        if player.phase == Player.Phase.BUY:
+    def desc(self, player: "Player.Player") -> str:
+        if player.phase == Phase.BUY:
             return """Exile a non-Victory card from the Supply. When you gain this, Exile a Gold from the Supply."""
         return "Exile a non-Victory card from the Supply."
 
-    def special(self, game, player):
+    def special(self, game: "Game.Game", player: "Player.Player") -> None:
         options = []
         for name, pile in game.get_card_piles():
             if pile.is_empty():
@@ -34,25 +34,27 @@ class Card_Camel_Train(Card.Card):
         if to_exile := player.plr_choose_options("Pick a card to Exile", *options):
             player.exile_card(to_exile)
 
-    def hook_gain_this_card(self, game, player):
+    def hook_gain_this_card(self, game: "Game.Game", player: "Player.Player") -> None:
         player.exile_card("Gold")
 
 
 ###############################################################################
 class TestCamelTrain(unittest.TestCase):
-    def setUp(self):
-        self.g = Game.TestGame(numplayers=1, initcards=["Camel Train"])
+    def setUp(self) -> None:
+        self.g = Game.TestGame(
+            numplayers=1, initcards=["Camel Train"], badcards=["Silver Mine"]
+        )
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.card = self.g.get_card_from_pile("Camel Train")
         self.plr.add_card(self.card, Piles.HAND)
 
-    def test_play(self):
+    def test_play(self) -> None:
         self.plr.test_input = ["Exile Silver"]
         self.plr.play_card(self.card)
         self.assertIn("Silver", self.plr.piles[Piles.EXILE])
 
-    def test_gain(self):
+    def test_gain(self) -> None:
         self.plr.gain_card("Camel Train")
         self.assertIn("Gold", self.plr.piles[Piles.EXILE])
 
