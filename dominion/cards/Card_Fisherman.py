b644397ee1e3561598f807a1990b7e51279a3d5c
Dougal Scott <dougal.scott@gmail.com>
2023-10-29 11:13:58 +1100

    More typing

--- dominion/cards/Card_Fisherman.py
+++ dominion/cards/Card_Fisherman.py
@@ -2,8 +2,7 @@
 """ http://wiki.dominionstrategy.com/index.php/Fisherman """
 
 import unittest
-from dominion import Game, Card, Piles
-import dominion.Card as Card
+from dominion import Game, Card, Piles, Phase, Player
 
 
 ###############################################################################
@@ -12,39 +11,46 @@ class Card_Fisherman(Card.Card):
         Card.Card.__init__(self)
         self.cardtype = Card.CardType.ACTION
         self.base = Card.CardExpansion.MENAGERIE
-        self.desc = "+1 Card; +1 Action; +1 Coin; During your turns, if your discard pile is empty, this costs 3 Coin less."
         self.name = "Fisherman"
         self.coin = 1
         self.cards = 1
         self.actions = 1
         self.cost = 5
 
-    def hook_this_card_cost(self, game, player):
+    def dynamic_description(self, player: "Player.Player") -> str:
+        if player.phase == Phase.BUY:
+            return """+1 Card; +1 Action; +1 Coin; During your turns, if your discard pile is empty, 
+            this costs 3 Coin less."""
+        return "+1 Card; +1 Action; +1 Coin"
+
+    def hook_this_card_cost(self, game: "Game.Game", player: "Player.Player") -> int:
         if player.piles[Piles.DISCARD].is_empty():
             return -3
         return 0
 
 
 ###############################################################################
-class Test_Fisherman(unittest.TestCase):
-    def setUp(self):
+class TestFisherman(unittest.TestCase):
+    def setUp(self) -> None:
         self.g = Game.TestGame(numplayers=1, initcards=["Fisherman"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.card = self.g.get_card_from_pile("Fisherman")
         self.plr.add_card(self.card, Piles.HAND)
 
-    def test_playcard(self):
+    def test_play_card(self) -> None:
         """Play the card"""
         self.plr.play_card(self.card)
         self.assertEqual(self.plr.coins.get(), 1)
         self.assertEqual(self.plr.actions.get(), 1)
         self.assertEqual(self.plr.piles[Piles.HAND].size(), 5 + 1)
 
-    def test_buycard(self):
+    def test_buy_card(self) -> None:
         """Buy the card"""
         self.plr.piles[Piles.DISCARD].set("Copper")
+        self.plr.phase = Phase.BUY
         self.assertEqual(self.plr.card_cost(self.card), 5)
+        self.assertIn("During", self.card.description(self.plr))
         self.plr.piles[Piles.DISCARD].set()
         self.assertEqual(self.plr.card_cost(self.card), 2)
 
