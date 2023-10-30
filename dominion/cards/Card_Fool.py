9ace6afe59383249e32c34f76bc19a2503e886fd
Dougal Scott <dougal.scott@gmail.com>
2023-10-31 10:05:01 +1100

    Improve testing

--- dominion/cards/Card_Fool.py
+++ dominion/cards/Card_Fool.py
@@ -1,54 +1,56 @@
 #!/usr/bin/env python
 
 import unittest
-from dominion import Game, Card, Piles
-import dominion.Card as Card
+from dominion import Game, Card, Piles, Player
+
+FOOL = "fool"
 
 
 ###############################################################################
 class Card_Fool(Card.Card):
-    def __init__(self):
+    def __init__(self) -> None:
         Card.Card.__init__(self)
         self.cardtype = [Card.CardType.ACTION, Card.CardType.FATE]
         self.base = Card.CardExpansion.NOCTURNE
-        self.desc = "If you aren't the player with Lost in the Woods, take it, take 3 Boons, and receive the Boons in any order."
+        self.desc = """If you aren't the player with Lost in the Woods, take it, take 3 Boons, 
+        and receive the Boons in any order."""
         self.name = "Fool"
         self.cost = 3
         self.heirloom = "Lucky Coin"
 
-    def special(self, game, player):
+    def special(self, game: "Game.Game", player: "Player.Player") -> None:
         if player.has_state("Lost in the Woods"):
             return
         player.assign_state("Lost in the Woods")
+        player.specials[FOOL] = True  # For testing
         for _ in range(3):
-            if not hasattr(player, "_fool_dont_boon"):
-                player.receive_boon()
-            else:
-                player._fool_dont_boon = True
+            player.receive_boon()
 
 
 ###############################################################################
-class Test_Fool(unittest.TestCase):
-    def setUp(self):
+class TestFool(unittest.TestCase):
+    def setUp(self) -> None:
         self.g = Game.TestGame(numplayers=1, initcards=["Fool"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.card = self.g.get_card_from_pile("Fool")
+        self.plr.specials[FOOL] = False
 
-    def test_play_with(self):
+    def test_play_with(self) -> None:
         """Play a Fool with Lost in the Woods"""
         self.plr.assign_state("Lost in the Woods")
-        self.plr._fool_dont_boon = False
+        self.plr.specials[FOOL] = False
         self.plr.add_card(self.card, Piles.HAND)
         self.plr.play_card(self.card)
-        self.assertFalse(self.plr._fool_dont_boon)
+        self.assertFalse(self.plr.specials[FOOL])
 
-    def test_play_without(self):
+    def test_play_without(self) -> None:
         """Play a Fool without Lost in the Woods"""
-        self.plr._fool_dont_boon = False
         self.plr.add_card(self.card, Piles.HAND)
+        self.plr.specials[FOOL] = False
+        self.plr.test_input = ["Finish", "Finish", "Finish"]
         self.plr.play_card(self.card)
-        self.assertTrue(self.plr._fool_dont_boon)
+        self.assertTrue(self.plr.specials[FOOL])
 
 
 ###############################################################################
--- dominion/states/State_Lost_in_Woods.py
+++ dominion/states/State_Lost_in_Woods.py
@@ -1,53 +1,59 @@
 #!/usr/bin/env python
 
 import unittest
-from dominion import Card, Game, Piles, State
+from dominion import Card, Game, Piles, State, Player
+
+LOST_IN_WOODS = "Lost in the Woods"
 
 
 ###############################################################################
 class State_Lost_in_woods(State.State):
-    def __init__(self):
+    def __init__(self) -> None:
         State.State.__init__(self)
         self.cardtype = Card.CardType.STATE
         self.base = Card.CardExpansion.NOCTURNE
-        self.desc = "At the start of your turn, you may discard a card to receive a Boon."
+        self.desc = (
+            "At the start of your turn, you may discard a card to receive a Boon."
+        )
         self.name = "Lost in the Woods"
         self.unique_state = True
 
-    def hook_start_turn(self, game, player):
-        dc = player.plr_discard_cards(prompt="Lost in the Woods: Discard a card to receive a boon")
-        if dc:
-            # Hack to make testing possible
-            if not hasattr(player, "_liw_dont_boon"):
-                player.receive_boon()
-            else:
-                player._liw_dont_boon = True
+    def hook_start_turn(self, game: "Game.Game", player: "Player.Player") -> None:
+        if player.plr_discard_cards(
+            prompt="Lost in the Woods: Discard a card to receive a boon"
+        ):
+            player.specials[LOST_IN_WOODS] = True  # For testing
+            player.receive_boon()
 
 
 ###############################################################################
-class Test_Lost_in_woods(unittest.TestCase):
-    def setUp(self):
+class TestLostInWoods(unittest.TestCase):
+    def setUp(self) -> None:
         self.g = Game.TestGame(quiet=True, numplayers=1, initcards=["Bard"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.state = self.g.states["Lost in the Woods"]
 
-    def test_lost_in_woods(self):
-        self.plr.piles[Piles.HAND].set("Copper", "Estate", "Gold")
+    def test_discard(self) -> None:
+        """Lost in the Woods and discard"""
+        self.plr.piles[Piles.HAND].set("Copper", "Estate")
         self.plr.assign_state("Lost in the Woods")
-        self.plr.test_input = ["Estate"]
-        self.plr._liw_dont_boon = False
+        # Finish is for the gained boon
+        self.plr.test_input = ["Discard Estate", "Finish"]
+        self.plr.specials[LOST_IN_WOODS] = False
         self.plr.start_turn()
-        self.assertTrue(self.plr._liw_dont_boon)
-        self.assertIsNotNone(self.plr.piles[Piles.DISCARD]["Estate"])
+        self.assertIn("Estate", self.plr.piles[Piles.DISCARD])
+        self.assertTrue(self.plr.specials[LOST_IN_WOODS])
 
-    def test_found_in_woods(self):
+    def test_dont_discard(self) -> None:
+        """Lost in the Woods and don't discard"""
         self.plr.piles[Piles.HAND].set("Copper", "Estate", "Gold")
         self.plr.assign_state("Lost in the Woods")
+        self.plr.specials[LOST_IN_WOODS] = False
         self.plr.test_input = ["Finish"]
-        self.plr._liw_dont_boon = False
         self.plr.start_turn()
-        self.assertFalse(self.plr._liw_dont_boon)
+        self.assertNotIn("Estate", self.plr.piles[Piles.DISCARD])
+        self.assertFalse(self.plr.specials[LOST_IN_WOODS])
 
 
 ###############################################################################
