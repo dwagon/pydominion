f23d5869cf53cf96c5c5e191ed05356bbf63d066
Dougal Scott <dougal.scott@gmail.com>
2023-11-15 10:20:58 +1100

    Handle running out of cards

--- dominion/cards/Card_Zombie_Spy.py
+++ dominion/cards/Card_Zombie_Spy.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 import unittest
-from dominion import Game, Card, Piles
-import dominion.Card as Card
+from dominion import Game, Card, Piles, Player, NoCardException
 
 
 ###############################################################################
-class Card_Zombie_Spy(Card.Card):
-    def __init__(self):
+class Card_ZombieSpy(Card.Card):
+    def __init__(self) -> None:
         Card.Card.__init__(self)
         self.cardtype = [Card.CardType.ACTION, Card.CardType.ZOMBIE]
         self.base = Card.CardExpansion.NOCTURNE
@@ -20,32 +19,35 @@ class Card_Zombie_Spy(Card.Card):
         self.cards = 1
         self.actions = 1
 
-    def setup(self, game):
+    def setup(self, game: Game.Game) -> None:
         game.trash_pile.add(self)
 
-    def special(self, game, player):
-        c = player.next_card()
-        discard = player.plr_choose_options(
+    def special(self, game: Game.Game, player: Player.Player) -> None:
+        try:
+            card = player.next_card()
+        except NoCardException:
+            return
+        if player.plr_choose_options(
             "Discard your card?",
-            ("Keep %s on your deck" % c.name, False),
-            ("Discard %s" % c.name, True),
-        )
-        if discard:
-            player.add_card(c, "discard")
-            player.output("Zombie Spy discarded your %s" % c.name)
+            (f"Keep {card} on your deck", False),
+            (f"Discard {card}", True),
+        ):
+            player.add_card(card, Piles.DISCARD)
+            player.output(f"Zombie Spy discarded your {card}")
         else:
-            player.add_card(c, "topdeck")
+            player.add_card(card, "topdeck")
 
 
 ###############################################################################
-class Test_Zombie_Spy(unittest.TestCase):
-    def setUp(self):
+class TestZombieSpy(unittest.TestCase):
+    def setUp(self) -> None:
         self.g = Game.TestGame(numplayers=1, initcards=["Zombie Spy"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.card = self.g.get_card_from_pile("Zombie Spy")
 
-    def test_play_keep(self):
+    def test_play_keep(self) -> None:
+        assert self.card is not None
         self.plr.test_input = ["Keep"]
         self.plr.piles[Piles.DECK].set("Province", "Estate")
         self.plr.play_card(self.card, discard=False, cost_action=False)
@@ -53,7 +55,8 @@ class Test_Zombie_Spy(unittest.TestCase):
         self.assertEqual(self.plr.actions.get(), 2)
         self.assertIn("Province", self.plr.piles[Piles.DECK])
 
-    def test_play_discard(self):
+    def test_play_discard(self) -> None:
+        assert self.card is not None
         self.plr.test_input = ["Discard"]
         self.plr.piles[Piles.DECK].set("Province", "Estate")
         self.plr.play_card(self.card, discard=False, cost_action=False)
