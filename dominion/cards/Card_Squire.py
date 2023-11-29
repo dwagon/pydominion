4824746c1f6a2bc4bbc040b4d83cfe5e182efb00
Dougal Scott <dougal.scott@gmail.com>
2023-11-29 13:53:17 +1100

    Hand running out of cards

--- dominion/cards/Card_Squire.py
+++ dominion/cards/Card_Squire.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import unittest
-from dominion import Game, Card, Piles
+from typing import Optional, Any
+
+from dominion import Game, Card, Piles, Player, NoCardException, OptionKeys
 
 
 ###############################################################################
 class Card_Squire(Card.Card):
-    def __init__(self):
+    def __init__(self) -> None:
         Card.Card.__init__(self)
         self.cardtype = Card.CardType.ACTION
         self.base = Card.CardExpansion.DARKAGES
@@ -16,7 +18,7 @@ class Card_Squire(Card.Card):
         self.cost = 2
         self.coin = 1
 
-    def special(self, game, player):
+    def special(self, game: Game.Game, player: Player.Player) -> None:
         choice = player.plr_choose_options(
             "Choose one.",
             ("+2 Actions", "actions"),
@@ -28,9 +30,14 @@ class Card_Squire(Card.Card):
         elif choice == "buys":
             player.buys.add(2)
         elif choice == "silver":
-            player.gain_card("Silver")
+            try:
+                player.gain_card("Silver")
+            except NoCardException:
+                player.output("No more Silver")
 
-    def hook_trash_this_card(self, game, player):
+    def hook_trash_this_card(
+        self, game: Game.Game, player: Player.Player
+    ) -> Optional[dict[OptionKeys, Any]]:
         attacks = []
         for name, _ in game.get_card_piles():
             card = game.card_instances[name]
@@ -39,19 +46,19 @@ class Card_Squire(Card.Card):
         cards = player.card_sel(prompt="Gain an attack card", cardsrc=attacks)
         if not cards:
             player.output("No suitable cards")
-            return
+            return {}
         player.gain_card(cards[0].name)
 
 
 ###############################################################################
 class TestSquire(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.g = Game.TestGame(numplayers=1, initcards=["Squire", "Militia"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         self.card = self.g.get_card_from_pile("Squire")
 
-    def test_play_actions(self):
+    def test_play_actions(self) -> None:
         """Play a Squire - gain actions"""
         self.plr.test_input = ["action"]
         self.plr.add_card(self.card, Piles.HAND)
@@ -61,7 +68,7 @@ class TestSquire(unittest.TestCase):
         self.assertEqual(self.plr.buys.get(), 1)
         self.assertNotIn("Silver", self.plr.piles[Piles.DISCARD])
 
-    def test_play_buys(self):
+    def test_play_buys(self) -> None:
         """Play a Squire - gain buys"""
         self.plr.test_input = ["buys"]
         self.plr.add_card(self.card, Piles.HAND)
@@ -70,7 +77,7 @@ class TestSquire(unittest.TestCase):
         self.assertEqual(self.plr.buys.get(), 3)
         self.assertNotIn("Silver", self.plr.piles[Piles.DISCARD])
 
-    def test_play_silver(self):
+    def test_play_silver(self) -> None:
         """Play a Squire - gain Silver"""
         self.plr.test_input = ["silver"]
         self.plr.add_card(self.card, Piles.HAND)
@@ -79,7 +86,7 @@ class TestSquire(unittest.TestCase):
         self.assertEqual(self.plr.buys.get(), 1)
         self.assertIn("Silver", self.plr.piles[Piles.DISCARD])
 
-    def test_trash(self):
+    def test_trash(self) -> None:
         """Trash a Squire"""
         self.plr.test_input = ["militia"]
         self.plr.trash_card(self.card)
