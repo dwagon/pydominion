004a068beb89b3972c21147dbcced1e3c5402dfe
Dougal Scott <dougal.scott@gmail.com>
2024-04-05 11:42:46 +1100

    Implemented Friendly

---
+++ dominion/traits/Trait_Friendly.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+""" https://wiki.dominionstrategy.com/index.php/Friendly"""
+import unittest
+from typing import Any
+
+from dominion import Card, Game, Trait, Piles, Player
+
+
+###############################################################################
+class Trait_Friendly(Trait.Trait):
+    """Friendly"""
+
+    def __init__(self) -> None:
+        Trait.Trait.__init__(self)
+        self.cardtype = Card.CardType.TRAITS
+        self.base = Card.CardExpansion.PLUNDER
+        self.desc = "At the start of your Clean-up phase, you may discard a Friendly card to gain a Friendly card."
+        self.name = "Friendly"
+
+    def hook_cleanup(self, game: "Game.Game", player: "Player.Player") -> None:
+        for card in player.piles[Piles.HAND]:
+            if self.name == game.card_piles[card.pile].trait:
+                if player.plr_choose_options(
+                    f"Friendly Trait lets you to gain another {card}.", ("Do nothing", False), (f"Gain a {card}", True)
+                ):
+                    player.gain_card(card.name)
+                break
+
+
+###############################################################################
+class Test_Friendly(unittest.TestCase):
+    """Test Friendly"""
+
+    def setUp(self) -> None:
+        self.g = Game.TestGame(quiet=True, numplayers=1, traits=["Friendly"], initcards=["Moat"])
+        self.g.start_game()
+        self.plr = self.g.player_list()[0]
+
+    def test_play(self) -> None:
+        """Check discarding Friendly cards"""
+        self.g.assign_trait("Friendly", "Moat")
+        self.plr.gain_card("Moat", Piles.HAND)
+        self.plr.test_input = ["Gain a Moat"]
+        self.plr.end_turn()
+        self.assertEqual(self.plr.piles[Piles.DISCARD].count("Moat"), 2)
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
