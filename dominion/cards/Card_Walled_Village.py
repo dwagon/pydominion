898c7c1b54ab1ba828ed27f9444e9af9b2868905
Dougal Scott <dougal.scott@gmail.com>
2024-04-14 17:19:06 +1000

    Implemented Walled Village

---
+++ dominion/cards/Card_Walled_Village.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+""" https://wiki.dominionstrategy.com/index.php/Walled_Village"""
+
+import unittest
+from dominion import Game, Card, Piles, Player, PlayArea
+
+
+WALLED_VILLAGE = "walled village"
+
+
+###############################################################################
+class Card_Walled_Village(Card.Card):
+    def __init__(self) -> None:
+        Card.Card.__init__(self)
+        self.cardtype = Card.CardType.ACTION
+        self.base = Card.CardExpansion.PROMO
+        self.desc = """+1 Card, +2 Actions; At the start of Clean-up, 
+        if you have this and no more than one other Action card in play, you may put this onto your deck"""
+        self.name = "Walled Village"
+        self.cards = 1
+        self.actions = 2
+        self.cost = 4
+
+    def hook_cleanup(self, game: Game.Game, player: Player.Player) -> None:
+        """At the start of Clean-up, if you have this and no more than one other Action card in play,
+        you may put this onto your deck"""
+        num_actions = sum(1 for card in player.piles[Piles.PLAYED] if card.isAction())
+        if num_actions > 2:
+            return
+        if WALLED_VILLAGE not in player.specials:
+            player.specials[WALLED_VILLAGE] = PlayArea.PlayArea(initial=None)
+        player.move_card(self, player.specials[WALLED_VILLAGE])
+        player.secret_count += 1
+
+    def hook_end_turn(self, game: "Game.Game", player: "Player.Player") -> None:
+        if WALLED_VILLAGE not in player.specials:
+            return
+        for card in player.specials[WALLED_VILLAGE]:
+            player.secret_count -= 1
+            player.add_card(card, player.piles[Piles.HAND])
+        player.specials[WALLED_VILLAGE] = PlayArea.PlayArea(initial=None)
+
+
+###############################################################################
+class Test_Walled_Village(unittest.TestCase):
+    def setUp(self) -> None:
+        self.g = Game.TestGame(numplayers=1, initcards=["Walled Village", "Moat"])
+        self.g.start_game()
+        self.plr = self.g.player_list()[0]
+        self.card = self.g.get_card_from_pile("Walled Village")
+        self.plr.add_card(self.card, Piles.HAND)
+
+    def test_play_card(self) -> None:
+        """Play a Walled Village"""
+        actions = self.plr.actions.get()
+        hand_size = self.plr.piles[Piles.HAND].size()
+        self.plr.play_card(self.card)
+        self.assertEqual(self.plr.actions.get(), actions + 2 - 1)
+        self.assertEqual(self.plr.piles[Piles.HAND].size(), hand_size + 1 - 1)
+        self.plr.end_turn()
+        self.plr.start_turn()
+        self.assertIn("Walled Village", self.plr.piles[Piles.HAND])
+
+    def test_play_no_return(self) -> None:
+        """Play a Walled Village so that we don't get it back"""
+        moat1 = self.g.get_card_from_pile("Moat")
+        moat2 = self.g.get_card_from_pile("Moat")
+        self.plr.play_card(self.card)
+        self.plr.play_card(moat1)
+        self.plr.play_card(moat2)
+        self.g.print_state()
+        self.plr.end_turn()
+        self.plr.start_turn()
+        self.assertNotIn("Walled Village", self.plr.piles[Piles.HAND])
+
+
+###############################################################################
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
+
+# EOF
