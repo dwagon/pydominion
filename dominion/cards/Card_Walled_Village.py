32f0c690a43173337b4ab23c41c8d2d27c318b1e
Dougal Scott <dougal.scott@gmail.com>
2024-04-15 15:55:45 +1000

    Fixed test failing randomly

--- dominion/cards/Card_Walled_Village.py
+++ dominion/cards/Card_Walled_Village.py
@@ -24,11 +24,13 @@ class Card_Walled_Village(Card.Card):
     def hook_cleanup(self, game: Game.Game, player: Player.Player) -> None:
         """At the start of Clean-up, if you have this and no more than one other Action card in play,
         you may put this onto your deck"""
+        if WALLED_VILLAGE not in player.specials:
+            player.specials[WALLED_VILLAGE] = PlayArea.PlayArea(initial=None)
         num_actions = sum(1 for card in player.piles[Piles.PLAYED] if card.isAction())
         if num_actions > 2:
             return
-        if WALLED_VILLAGE not in player.specials:
-            player.specials[WALLED_VILLAGE] = PlayArea.PlayArea(initial=None)
+        if self.name not in player.piles[Piles.PLAYED]:
+            return
         player.move_card(self, player.specials[WALLED_VILLAGE])
         player.secret_count += 1
 
@@ -64,7 +66,9 @@ class Test_Walled_Village(unittest.TestCase):
     def test_play_no_return(self) -> None:
         """Play a Walled Village so that we don't get it back"""
         # Fill discard so we don't need to shuffle the Walled Village back in to refill hand
-        self.plr.piles[Piles.DISCARD].set("Copper", "Silver", "Gold", "Estate", "Duchy")
+        self.plr.piles[Piles.DECK].set(
+            "Copper", "Silver", "Gold", "Estate", "Duchy", "Province", "Copper", "Silver", "Gold", "Estate"
+        )
         moat1 = self.g.get_card_from_pile("Moat")
         moat2 = self.g.get_card_from_pile("Moat")
         self.plr.play_card(self.card)
@@ -72,7 +76,6 @@ class Test_Walled_Village(unittest.TestCase):
         self.plr.play_card(moat2)
         self.plr.end_turn()
         self.plr.start_turn()
-
         self.assertNotIn("Walled Village", self.plr.piles[Piles.HAND])
 
 
