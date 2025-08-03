53bd8193deb88858cb63914cc8dfdb090d8dc877
Dougal Scott <dougal.scott@gmail.com>
2025-08-03 19:24:04 +1000

    Handle change of website

--- contrib/cardset_grabber.py
+++ contrib/cardset_grabber.py
@@ -1,48 +1,44 @@
 #!/usr/bin/env python
-""" Grab recommended sets of cards from wiki.dominionstrategy.com """
+"""Grab recommended sets of cards from wiki.dominionstrategy.com"""
 import argparse
 import string
-import sys
-from typing import Optional
+import time
 
-import requests
-from bs4 import BeautifulSoup
+import httpx
+import bs4
 
 
 ##############################################################################
 def get_html(url: str) -> str:
     """Get the HTML from the url"""
-    req = requests.get(url)
+    req = httpx.get(url, follow_redirects=True)
     req.raise_for_status()
     return req.text
 
 
 ##############################################################################
-def get_set_name(item) -> Optional[str]:
-    """Get the name of the set"""
-    for kid in item.descendants:
-        if kid.name is None:
-            if kid.startswith(" ") and kid.endswith(" "):
-                return kid.text.strip()
-    print(f"Couldn't find set name in {item}", file=sys.stderr)
-    return None
+def get_set_name(item: bs4.PageElement) -> str:
+    """Get the name of the set from
+        <tr>
+        <th colspan="15" style="position:relative">Victory Dance <span class="mw-customtoggle-Victory_Dance" style="position:absolute;
+    right:2px;">
+        [<a href="#¬"><span>images</span></a>]</span>
+        </th>
+        </tr>"""
+    try:
+        return str(item.th.contents[0].strip())
+    except:
+        return ""
 
 
 ##############################################################################
-def get_card_name(item) -> Optional[str]:
-    """Get the card name"""
-    return item.text.strip()
-
-
-##############################################################################
-def save_set(name, cards, expansion):
+def save_set(name: str, cards: list[str], expansion: str) -> None:
     """Save the set"""
     fname = name.replace(" ", "_")
     fname = fname.replace("&", "_and_")
     fname = fname.replace("'", "")
-    print(f"Saving {fname} from {expansion}", file=sys.stderr)
     with open(fname, "w", encoding="utf-8") as outfh:
-        outfh.write(f"# {expansion}\n")
+        outfh.write(f"# {expansion} {time.ctime()}\n")
         for card in sorted(cards):
             card = card.strip()
             if not card:
@@ -53,7 +49,7 @@ def save_set(name, cards, expansion):
 
 
 ##############################################################################
-def parse_table(table, expansion):
+def parse_table(table: bs4.PageElement, expansion: str) -> None:
     """Parse the table recommended sets of 10 section"""
     cards = []
     set_name = None
@@ -61,41 +57,41 @@ def parse_table(table, expansion):
         if not set_name and item.name == "tr":
             set_name = get_set_name(item)
     if not set_name:
+        print("No set name found")
         return
     for item in table.find_all("a"):
-        cards.append(get_card_name(item))
+        if card_name := item.string:
+            cards.append(card_name.strip())
     if set_name[0] in string.ascii_uppercase:
         save_set(set_name, cards, expansion)
 
 
 ##############################################################################
-def get_expansion():
+def get_expansion() -> str:
     """Process args to get expansion"""
     parser = argparse.ArgumentParser(description="Get Recommended Sets")
     parser.add_argument("expansion")
     args = parser.parse_args()
-    return args.expansion
+    return str(args.expansion)
 
 
 ##############################################################################
-def main():
+def main() -> None:
     """Main"""
     expansion = get_expansion()
-    url = f"http://wiki.dominionstrategy.com/index.php/{expansion}"
+    print(f"Scanning {expansion}")
+    url = f"https://wiki.dominionstrategy.com/index.php/Recommended_Kingdoms/{expansion}"
     try:
         html = get_html(url)
-    except requests.exceptions.HTTPError as exc:
+    except httpx.HTTPError as exc:
         print(f"Couldn't get {url}\n\t{exc}")
         return
 
-    soup = BeautifulSoup(html, "html.parser")
+    soup = bs4.BeautifulSoup(html, "html.parser")
     for h2 in soup.find_all("h2"):
-        if "Recommended sets " in h2.text:
-            for item in h2.next_elements:
-                if item.name == "table":
-                    parse_table(item, expansion)
-                if item.name == "h2":
-                    break
+        for item in h2.next_elements:
+            if item.name == "table":
+                parse_table(item.tbody, expansion)
 
 
 ##############################################################################
