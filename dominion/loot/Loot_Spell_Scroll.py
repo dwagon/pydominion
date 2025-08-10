653168132052e891c191386e522549825ac8ab7e
Dougal Scott <dougal.scott@gmail.com>
2025-08-11 09:50:21 +1000

    Bad test interaction

--- dominion/loot/Loot_Spell_Scroll.py
+++ dominion/loot/Loot_Spell_Scroll.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-""" https://wiki.dominionstrategy.com/index.php/Spell_Scroll"""
+"""https://wiki.dominionstrategy.com/index.php/Spell_Scroll"""
 import unittest
 
 from dominion import Loot, Card, Game, Piles
@@ -24,17 +24,13 @@ class Loot_SpellScroll(Loot.Loot):
 
     def special(self, game, player):
         """Trash this to gain a cheaper card. If it's an Action or Treasure, you may play it."""
-        to_trash = player.plr_choose_options(
-            "Trash the Spell Scroll?", ("Trash it", True), ("Keep it", False)
-        )
+        to_trash = player.plr_choose_options("Trash the Spell Scroll?", ("Trash it", True), ("Keep it", False))
         if not to_trash:
             return
         player.trash_card(self)
         gained_card = player.plr_gain_card(cost=6)
         if gained_card.isAction() or gained_card.isTreasure():
-            to_play = player.plr_choose_options(
-                f"Play {gained_card}?", ("Play", True), ("Discard", False)
-            )
+            to_play = player.plr_choose_options(f"Play {gained_card}?", ("Play", True), ("Discard", False))
             if to_play:
                 player.play_card(gained_card, cost_action=False, discard=False)
 
@@ -44,7 +40,7 @@ class TestSpellScroll(unittest.TestCase):
     """Test Spell Scroll"""
 
     def setUp(self):
-        self.g = Game.TestGame(quiet=True, numplayers=1, traits=["Cursed"])
+        self.g = Game.TestGame(quiet=True, numplayers=1, traits=["Cursed"], badcards=["Gold Mine"])
         self.g.start_game()
         self.plr = self.g.player_list()[0]
         # Remove all other cards from loot pile, so we know what we will draw
