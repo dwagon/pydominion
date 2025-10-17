80690d95374e55437a0b0879374702a3642f8f36
Dougal Scott <dougal.scott@gmail.com>
2025-10-18 10:07:58 +1100

    Pick cards from suitable cards

--- dominion/cards/Card_War_Chest.py
+++ dominion/cards/Card_War_Chest.py
@@ -1,68 +1,71 @@
 #!/usr/bin/env python
-"""http://wiki.dominionstrategy.com/index.php/War Chest"""
+"""https://wiki.dominionstrategy.com/index.php/War_Chest"""
 
 import unittest
 from typing import Any
 
-from dominion import Piles, NoCardException
-from dominion.Card import Card, CardType, CardExpansion
-from dominion.Game import Game, TestGame
-from dominion.Player import Player
+from dominion import Piles, NoCardException, Card, Game, Player
 
 WAR_CHEST = "war chest"
 
 
 ###############################################################################
-class Card_WarChest(Card):
+class Card_War_Chest(Card.Card):
     """War Chest"""
 
     def __init__(self) -> None:
-        Card.__init__(self)
-        self.cardtype = CardType.TREASURE
-        self.base = CardExpansion.PROSPERITY
+        Card.Card.__init__(self)
+        self.cardtype = Card.CardType.TREASURE
+        self.base = Card.CardExpansion.PROSPERITY
         self.desc = """The player to your left names a card.
         Gain a card costing up to $5 that hasn't been named for War Chest this turn."""
         self.name = "War Chest"
         self.cost = 5
 
-    def hook_start_turn(self, game: Game, player: Player) -> None:
+    def hook_start_turn(self, game: Game.Game, player: Player.Player) -> None:
         player.specials[WAR_CHEST] = []
 
-    def special(self, game: Game, player: Player) -> None:
+    def special(self, game: Game.Game, player: Player.Player) -> None:
         if WAR_CHEST not in player.specials:
             player.specials[WAR_CHEST] = []
 
-        lefty = game.player_to_left(player)
-        options = [(f"Pick {card}", card.name) for card in player.cards_under(5) if card.purchasable]
-        banned = lefty.plr_choose_options(
-            f"Name a card that {player.name} can't gain from a War Chest this turn",
-            *options,
-        )
+        banned = get_banned_card(game, player)
         player.specials[WAR_CHEST].append(banned)
-        card_name = player.plr_choose_options("Pick a card to gain", *self.generate_options(game, player))
+        card_name = player.plr_choose_options("Pick a card to gain", *generate_war_chest_options(player))
         try:
             player.gain_card(card_name)
         except NoCardException:
             player.output(f"No more {card_name}")
 
-    @classmethod
-    def generate_options(cls, game: Game, player: Player) -> list[tuple[str, str]]:
-        """Generate list of options to select cards"""
-        options: list[tuple[str, str]] = []
-        for name, _ in game.get_card_piles():
-            if name in player.specials[WAR_CHEST]:
-                continue
-            card = game.card_instances[name]
-            if player.card_cost(card) > 5:
-                continue
-            options.append((f"Get {name}", name))
-        return options
+
+###############################################################################
+def get_banned_card(game: Game.Game, player: Player.Player) -> str:
+    """Return the banned card that can't be selected"""
+    lefty = game.player_to_left(player)
+    options = [(f"Pick {card}", card.name) for card in player.cards_under(5) if card.purchasable]
+    banned = lefty.plr_choose_options(
+        f"Name a card that {player} can't gain from a War Chest this turn",
+        *options,
+    )
+    return banned
+
+
+###############################################################################
+def generate_war_chest_options(player: Player.Player) -> list[tuple[str, str]]:
+    """Generate list of options to select cards"""
+    options = [
+        (f"Gain {card}", card.name)
+        for card in player.cards_under(5)
+        if card.purchasable and card.name not in player.specials[WAR_CHEST]
+    ]
+    return options
 
 
 ###############################################################################
 def botresponse(  # pragma: no coverage, pylint: disable=unused-argument
-    player: Player, kind: str, args: Any = None, kwargs: Any = None
+    player: Player.Player, kind: str, args: Any = None, kwargs: Any = None
 ) -> Any:
+    """Name a generally useful card"""
     return "Silver"
 
 
@@ -71,7 +74,7 @@ class TestWarChest(unittest.TestCase):
     """Test War Chest"""
 
     def setUp(self) -> None:
-        self.g = TestGame(numplayers=2, initcards=["War Chest"], badcards=["Silver Mine"])
+        self.g = Game.TestGame(numplayers=2, initcards=["War Chest"], badcards=["Silver Mine"])
         self.g.start_game()
         self.plr, self.oth = self.g.player_list()
         self.card = self.g.get_card_from_pile("War Chest")
@@ -80,7 +83,7 @@ class TestWarChest(unittest.TestCase):
         """Play this card"""
         self.plr.add_card(self.card, Piles.HAND)
         self.oth.test_input = ["Pick Duchy"]
-        self.plr.test_input = ["Get Silver"]
+        self.plr.test_input = ["Gain Silver"]
         self.plr.play_card(self.card)
         self.assertIn("Silver", self.plr.piles[Piles.DISCARD])
 
